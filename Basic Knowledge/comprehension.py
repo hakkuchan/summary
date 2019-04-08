fe174d06e7140b013fcadd3aa613c13090b57680
ZhouBin <zhoubin_chn@163.com>
2019-04-08 11:09:44 +0800

    20190408

--- Basic Knowledge/comprehension.py
+++ Basic Knowledge/comprehension.py
@@ -6,6 +6,6 @@ print([(x, y) for x in [1,2,3] for y in [3,1,4] if x != y])
 print([round(math.pi, i) for i in range(1, 6)])
 
 """ 字典推导式 """
-a = {"a":1, "b":2, "c":3, "d":4}
-print(a)
-print({v:k for k,v in a.items()})
+eg_dict = {"a":1, "b":2, "c":3, "d":4}
+print(eg_dict)
+print({v+100:k for k,v in eg_dict.items()})
