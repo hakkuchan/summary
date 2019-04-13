a324bafb21ed2e5f399b94300d35b8c91179037c
ZhouBin <zhoubin_chn@163.com>
2019-04-13 10:34:46 +0800

    20190413

--- 1. Python基础/函数.py
+++ 1. Python基础/函数.py
@@ -8,7 +8,6 @@
 
 """ 传递函数 """
 # 传递函数的关键在于函数名后的括号，没有括号，函数就不会被执行，而可以被传递
-
 def fn(a,b):
     return a+b
 fn1 = fn
@@ -19,7 +18,6 @@ print(fn1(1,2), fn2(3,4), fn(5,6))
 
 """ * 的作用 """
 # (1) 使用函数时，如果函数有多个参数，可用 fun(*[参数列表]) 的形式传入参数
-
 def func(a, b):
     return a + b
 param = [2,3]
@@ -27,7 +25,6 @@ out = func(*param)
 print(out)
 
 # (2) 定义函数时，如果函数的参数不确定，可 def fun(*x)，使用函数时传入的所有参数都会保存在 x（tuple形式）中
-
 def func(*x):
     return x
 out = func(1,2,3,4,5,6) 
@@ -36,7 +33,6 @@ print(out)
 
 """ ** 的作用 """
 # 定义函数时，使用 **参数 可以把字典作为参数传递给一个函数。
-
 def student_grade(**kwargs):
     for key, value in kwargs.items():
         print("{}'s grade is' {}".format(key, value))
@@ -44,14 +40,18 @@ student_grade(**{'Jack':98, 'Mike':99, 'Lily':97})
 
 
 """ 匿名函数：lambda表达式 """
-
-f = lambda x: x ** 2 + 2 * x     # x为入口参数，x ** 2 + 2 * x 为函数体
+# x为入口参数，x ** 2 + 2 * x 为函数体
+f = lambda x: x ** 2 + 2 * x
 print(f(2))   
 
 # 等价于：
 def g(x):
     return x ** 2 + 2 * x
 print(,g(2))
+
+# lambda表达式也可以定义多个变量
+f = lambda x, y: x + y
+print(f(2,3))
 
 
 """ 函数标注 """
