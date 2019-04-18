bd6c8029ff51f957ad01ecfb19cd912d250ba1ad
ZhouBin <zhoubin_chn@163.com>
2019-04-18 10:03:28 +0800

    20190418

--- 2. 常用模块和计算/读写文件.py
+++ 2. 常用模块和计算/读写文件.py
@@ -1,49 +1,50 @@
-""" 处理 csv 文件 """
+""" 读写 csv 文件 """
+import math
 import pandas as pd
-# 读取 csv 文件为 dataframe 格式
-data = pd.read_csv('E:\Work\Jupyter\data\char_data.csv') 
+
+# 读取 csv 文件
+oilshale_table = pd.read_csv('E:\Work\Jupyter\data\char_data.csv') 
+
 # 如果需要添加表头：
 name = ['preg','plas','pres','skin','test','mass','pedi','age','class']
-diabetes_table = pd.read_csv('E:\DataAnalysis\Library\pima-indians-diabetes.csv',names=name)
+diabetes_table = pd.read_csv('E:\Work\Jupyter\data\pima-indians-diabetes.csv',names=name)
 
 # 将 Dataframe 格式写入csv文件，index=True 保留行索引
-data.to_csv('123.csv', index=True)
+oilshale_table.to_csv('filename.csv', index=True)
 
 
-""" 处理文本文件 """
 
-""" 打开 - 读取文件 """
-with open('E:\Work\Jupyter/test.txt', 'r') as f:  # 指针'r'表示以只读方式打开文件
-    print(f.read())  # .read() 表示读取文件整体
+""" 读写文本文件 """
 
-with open('E:\Work\Jupyter/test.txt', 'r') as f:
+# 读取
+with open('E:\Work\Jupyter\data/read_text.txt', 'r') as f:
+    data = f.read()  # .read() 表示读取文件整体
+print(data)
+
+with open('E:\Work\Jupyter\data/read_text.txt', 'r') as f:
     for line in f:
         print(line)  # 逐行读取 
-
-with open('E:\Work\Jupyter/test.txt', 'r') as f:
-    for ele in f.read():
-        print(ele)   # 逐字读取
-		
-""" 打开 - 写入文件"""
-with open('E:\Work\Jupyter/test.txt', 'w') as f:  # 指针'w'表示以写入模式
-    f.write('123')
+    
+# 写入
+with open('E:\Work\Jupyter\data/write_text.txt', 'w') as f:
+    print('Hello World!', file=f, end=' ')    # end 控制换行，默认换行
+    print(math.pi, math.e, file=f, sep='|')   # sep 设定打印元素之间的符号，默认为空格
 
 
-"""
-指针的含义：
+""" 
 
+读写文本文件的总结：
+
+with open('文件目录', '指针') as f: 创造上下文环境，with 控制块结束时，文件会自动关闭。
+
+文件目录中的 ‘\’ 和 ‘/’ 需要区分：目录之间用‘\’；文件与目录之间用‘/’，例如：'E:\Work\Jupyter\data/write_text.txt'
+
+指针规定文件操作类型，常用如下：
+ 
 r : 读取文件，若文件不存在则会报错
 
 w: 写入文件，若文件不存在则会先创建再写入，会覆盖原文件
 
 a : 写入文件，若文件不存在则会先创建再写入，但不会覆盖原文件，而是追加在文件末尾
-
-rb,wb：分别于r,w类似，但是用于读写二进制文件
-
-r+ : 可读、可写，文件不存在也会报错，写操作时会覆盖
-
-w+ : 可读，可写，文件不存在先创建，会覆盖
-
-a+ ：可读、可写，文件不存在先创建，不会覆盖，追加在末尾
 
 """
