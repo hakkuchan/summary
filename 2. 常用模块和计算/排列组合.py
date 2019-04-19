5df90c80a562550150de2ae9976eb4db62a55028
ZhouBin <zhoubin_chn@163.com>
2019-04-19 08:42:40 +0800

    20190419

--- 2. 常用模块和计算/排列组合.py
+++ 2. 常用模块和计算/排列组合.py
@@ -6,7 +6,11 @@ param_idx = np.array([1,2,3,4,5])
 # 有序排列
 for comb in itertools.combinations(param_idx, 3):
     print(list(comb))
-    
+
 # 无序排列（全排列）
 for comb in itertools.permutations(param_idx, 3):
     print(list(comb))
+	
+# 对若干列表中的元素进行有序排列
+comb = list(itertools.product([1,2,3], [4,5,6], [7,8,9]))
+print(comb)
