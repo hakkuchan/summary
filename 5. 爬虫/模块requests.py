9926c1777ccc789b6aa75191b6a05920903dcee2
ZhouBin <zhoubin_chn@163.com>
2019-04-29 10:48:00 +0800

    20190429

--- 5. 爬虫/模块requests.py
+++ 5. 爬虫/模块requests.py
@@ -1,7 +1,8 @@
 #coding=utf-8
 import requests
+from requests.auth import HTTPBasicAuth
 
-""" 请求 """
+""" 基本请求 """
 html = requests.get('https://maoyan.com/board/4')
 print(html.status_code)
 # status_code 状态码：
@@ -10,6 +11,10 @@ print(html.status_code)
 # 3开头-请求被重定向；
 # 4开头-请求错误；
 # 5开头 - 服务器错误 
+
+""" 输入用户名和密码 """
+html = requests.get('website', auth=('username', 'password'))
+print(html.status_code)
 
 """ 获取响应内容 """
 html = html.content
