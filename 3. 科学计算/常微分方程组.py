d9c4f6a18a359d7161e631b1ff7abb890179ab39
ZhouBin <zhoubin_chn@163.com>
2019-06-06 08:39:24 +0800

    20190606

---
+++ 3. 科学计算/常微分方程组.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+%matplotlib inline
+
+"""
+常微分方程组（以洛伦兹方程为例）：
+dx/dt = p(y-x)
+dy/dt = x(r-z)-y
+dz/dt = xy - bz
+"""
+
+""" 定义常微分方程组 """
+def dmove(point,t,sets):
+    """
+    point：位置矢量
+    t: 时间
+    sets：其他参数
+    """
+    p,r,b = sets
+    x,y,z = point
+    return np.array([p*(y-x),x*(r-z)-y,x*y-b*z])
+ 
+""" 调用 odeint 对常微分方程组 dmove 进行求解 """
+t = np.arange(0,30,0.01)  # 设定时间
+out = odeint(dmove,(0.,1.,0.),t,args = ([10,30,3],)) 
+# 其中，(0.,1.,0.)是point的初值; 
+# args设定的是常微分方程组中的参数 p,r,b
+# args = ([10,30,3],)中的逗号不可省略
+
+""" 画3维空间的曲线 """ 
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot(out[:,0],out[:,1],out[:,2], color='red', linewidth=1) # out[:,0],out[:,1],out[:,2] 分别是不同 t 下的 x, y, z
+plt.show()
