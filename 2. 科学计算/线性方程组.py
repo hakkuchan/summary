084f2c2266cf34e417d74cd2fb35683ac71f33c3
ZhouBin <zhoubin_chn@163.com>
2019-06-08 10:05:46 +0800

    20190608

--- 2. 科学计算/线性方程组.py
+++ 2. 科学计算/线性方程组.py
@@ -9,18 +9,19 @@ from scipy.optimize import fsolve
 x1 + x2 + x3 = 4
 """
 
-""" 方法 1：利用 linalg """
+""" 方法 1：利用 numpy 中的 linalg """
 A=np.array([[3,2,1],[2,4,3],[1,1,1]])
 B=np.array([8,13,4])
 x=linalg.solve(A,B)
 print('方法 1 结果:',x)
 
-""" 方法 2：利用 fsolve """
+""" 方法 2：利用 scipy 中的 fsolve """
 def fn(x):
+    x1,x2,x3 = x.tolist()
     return np.array([
-                     3 * x[0] + 2 * x[1] + x[2] - 8,
-                     2 * x[0] + 4 * x[1] + 3 * x[2] -13, 
-                     x[0] + x[1] + x[2] - 4
+                     3 * x1 + 2 * x2 + x3 - 8,
+                     2 * x1 + 4 * x2 + 3 * x3 -13, 
+                     x1 + x2 + x3 - 4
                     ])
 
 x = fsolve(fn,[0,0,0])
