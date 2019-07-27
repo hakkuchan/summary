f2d7935add133f2d497d817f16c5a8061300f901
ZhouBin <zhoubin_chn@163.com>
2019-07-27 12:22:15 +0800

    20190727

--- 1. Python基础/装饰器.py
+++ 1. Python基础/装饰器.py
@@ -1,6 +1,6 @@
 """ 
-装饰器本质上是一个以别的函数或类为输入的函数，
-在不改变原函数或类代码的情况下，为它们增加新功能
+装饰器本质上是以已有函数或类为输入的函数，
+目的是在不改变原函数或类代码的情况下，为它们增加新功能
 """
 
 """ 定义装饰器 """
@@ -8,20 +8,17 @@ def show_inputs(fn):    # 定义一个名为 show_input 的装饰器，该装饰
     def new_operation(a,b,c):   # 定义新功能
         print("input:", a,b,c)  # 新功能：输出传入函数的输入值
         return fn(a,b,c)   # 执行原函数的操作
-    return new_operation
+    return new_operation   # <?> 还不太明白
 
 """ 应用 """
-
-# 方法1：把现有函数 (square_sum for example) 代入装饰器函数
+# 方法1（反映了装饰的本质）：
 def square_sum(a, b, c):
     return a**2 + b**2 + c**2
-
 new_func = show_inputs(square_sum)
 print(new_func(3,4,5))
 
-# 方法2：魔法函数
+# 方法2（常见用法）：
 @show_inputs
 def square_sum(a, b, c):
     return a**2 + b**2 + c**2
-
 print(square_sum(3,4,5))
