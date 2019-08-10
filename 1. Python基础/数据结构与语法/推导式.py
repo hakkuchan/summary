5f98df7dd8a023aaf2fed57342b749900147a51e
ZhouBin <zhoubin_chn@163.com>
2019-08-10 20:28:00 +0800

    20190810

--- 1. Python基础/数据结构与语法/推导式.py
+++ 1. Python基础/数据结构与语法/推导式.py
@@ -3,17 +3,26 @@ x = -1
 out = 0 if x < 0 else x
 print(out)
 
-print(0 if x < 0 else x) # 也可直接输出推导式
+print(0 if x < 0 else x)  # 也可直接输出推导式
+
 
 """ 列表推导式 """
-import math
-print([x**2 for x in range(10)])
+lst = [x**2 for x in range(10)]
+print(lst)
+
+print([x**2 for x in range(10)])  # 也可直接输出推导式
+
 print([[x**2, 3*x] for x in range(10)])
+print([round(3.1415926, i) for i in range(1, 6)])
+
 print([(x, y) for x in [1,2,3] for y in [3,1,4] if x != y])
-print([round(math.pi, i) for i in range(1, 6)])
+
 
 """ 字典推导式 """
-print({i:j for i,j in zip(['a','b','c'], [1,2,3])})
+dic = {i:j for i,j in zip(['a','b','c'], [1,2,3])}
+print(dic)
 
-eg_dict = {"a":1, "b":2, "c":3, "d":4}
-print({v+100:k for k,v in eg_dict.items()})
+print({i:j for i,j in zip(['a','b','c'], [1,2,3])})  # 也可直接输出推导式
+
+dic = {"a":1, "b":2, "c":3, "d":4}
+print({v+100:k for k,v in dic.items()})
