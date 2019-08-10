718ec059a12a9fe28af2d19e5d8d7b910b409685
ZhouBin <zhoubin_chn@163.com>
2019-08-10 20:20:32 +0800

    20190810

--- 1. Python基础/数据结构与语法/流程控制.py
+++ 1. Python基础/数据结构与语法/流程控制.py
@@ -1,45 +1,41 @@
-""" 条件语句: (1) if……else……  (2) if……elif……elif……  (3) 字典的应用 """
+"""
+1. 条件语句: 
+    (1) if……else……  
+    (2) if……elif……elif……  
 
-# 字典取代多个 elif
-def fun(x):
-    if x == 1:
-        print('one')
-    elif x == 2:
-        print('two')
-    elif x == 3:
-        print('three')
-# 等价于:
-def fun(x):
-    return {1:'one', 2:'two', 3:'three'}.get(x)
-
-
-""" 循环语句: (1) while 布尔条件 (2) for 范围条件，包括 2 种用法：for i in range(num) 和 for i in [1, 2, 3] """
-
-""" 三种特殊循环方法: (1) items() 取出字典的键值对; (2) enumerate() 取出索引和元素; (3) zip() 同时取出多个容器的元素"""
-
-''' items() 示例 '''
-knights = {'gallahad': 'the pure', 'robin': 'the brave'}
-for k, v in knights.items():
-    print(k, v)
-
-''' enumerate() 示例 '''
-for i, v in enumerate(['tic', 'tac', 'toe']):
-    print(i, v)
-
-''' zip() 示例 '''
-questions = ['name', 'quest', 'favorite color']
-answers = ['lancelot', 'the holy grail', 'blue']
-for q, a in zip(questions, answers):
-     print(('What is your {0}? It is {1}.'.format(q, a)))
-
-""" break 和 continue: 
-    break 用于终止最近的 for 或 while 循环，执行之后的代码; 
-    continue 跳过当前循环的剩余语句，然后继续进行下一轮循环。
-	慎用break和continue。
+2. 循环语句: 
+    (1) while 布尔条件 
+    (2) for 范围条件，包括 2 种用法：
+        (a) for i in range(num)
+        (b) for i in [1, 2, 3]
+        
+3. break 和 continue 语句：
+    (1) break 用于终止最近的 for 或 while 循环，执行之后的代码; 
+    (2) continue 跳过当前循环的剩余语句，然后继续进行下一轮循环。
+    (3) 慎用break和continue。
+    
+4. pass 语句：
+    当语法上需要一个语句，但程序需要什么动作也不做时，可以使用 pass。
 """
 
-""" pass 当语法上需要一个语句，但程序需要什么动作也不做时，可以使用 pass。"""
+""" 3种特殊循环"""
 
-""" 输入函数 input() """
-score = input('请输入成绩：') # 注意：input()返回结果都为字符串，如果需要变为数字则用到int()/float()
+# items() 取出字典的键值对
+knights = {'Mike': 99, 'Julia': 97}
+for k, v in knights.items():
+    print(k, v)
+    
+# enumerate() 取出索引和元素
+for i, j in enumerate(['a', 'b', 'c']):
+    print(i, j)
+    
+# zip() 同时取出多个容器的元素
+questions = ['name', 'age']
+answers = ['Mike', '27']
+for a, b in zip(questions, answers):
+     print((f'What is your {a}?  It is {b}.'))
+        
+""" input()函数 """
+score = input('请输入成绩：')  # 注意：input()返回结果都为字符串
 print('该学生成绩为：' + score)
+print(type(score))
