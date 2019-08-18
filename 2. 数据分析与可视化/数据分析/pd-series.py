7eb45d0c36a095e61f6e2d5f29da328722c25653
ZhouBin <zhoubin_chn@163.com>
2019-08-18 09:37:22 +0800

    20190818

--- 2. 数据分析与可视化/数据分析/pd-series.py
+++ 2. 数据分析与可视化/数据分析/pd-series.py
@@ -1,26 +1,19 @@
-""" 
-pandas中的series
-
-是带有标签的一维数组，
-
-可以保存任何数据类型（整数，字符串，浮点数，Python对象等）,
-
-轴标签统称为索引
-
-"""
 import numpy as np
-import pandas as pd  
+import pandas as pd
 
-''' (1) 概述 ''' 
-s = pd.Series(np.random.rand(5))
-print(s)        # 查看series
-print(type(s))  # 查看series类型
-print(s.index,type(s.index))   # 查看series索引
-print(s.values,type(s.values)) # 查看series值
-# 核心：series相比于ndarray，是一个自带索引index的数组 → 一维数组 + 对应索引
-# 所以当只看series的值的时候，就是一个一维数组
-# series和ndarray较相似，索引切片等功能差别不大
-# series和dict相比，series更像一个有顺序的字典（dict本身不存在顺序），其索引原理与字典相似（一个用key，一个用index)
+
+''' (1) 概述 
+    series 是带有标签的一维数组
+    可以保存任何数据类型（整数，字符串，浮点数，Python对象等）
+    轴标签统称为索引，从0开始
+    当只看 series 的值的时候，就是一个一维数组
+    series 和 ndarray 较相似，索引切片等功能差别不大 '''
+
+s = pd.Series(np.random.randint(0,3,5))
+print(s)         # 查看series
+print(type(s))   # 查看series类型
+print(s.index)   # 查看series索引
+print(s.values)  # 查看series值
 
 
 ''' (2) 创建方法 '''
@@ -29,20 +22,22 @@ print(s.values,type(s.values)) # 查看series值
 print(pd.Series({'a':1 ,'b':2 , 'c':3, '4':4, '5':5}))
 
 # b. 由数组创建
-print(pd.Series(np.random.randn(5)))
-print(pd.Series(np.random.randn(5), index = ['a','b','c','d','e'], dtype = np.object)) # 更换索引和数据类型
+print(pd.Series(np.random.randint(0, 3, 5)))
+print(pd.Series(np.random.randint(0, 3, 5), index = ['a','b','c','d','e'], dtype = np.object)) # 更换索引和数据类型
 
 # c. 由标量创建
-print(pd.Series(10, index = range(4)))
+print(pd.Series([10,11,12,13], index = range(4)))
+
 
 ''' (3) 索引 '''
-s = pd.Series(np.random.rand(5), index = ['a','b','c','d','e'])
+s = pd.Series(np.random.random(5), index = ['a','b','c','d','e'])
+print(s)
 
 # a. 位置索引
 print(s[1])
 
 # b. 标签索引
-print(s['b'])
+print(s['c'])
 
 # c. 切片索引
 print(s[1::])
@@ -54,13 +49,15 @@ print(s['a':'c'],s['c'])
 s = pd.Series(np.random.rand(4)*100)
 s[4] = None       # 添加一个空值
 print(s)
+# I. 按条件筛选
 bs1 = s > 50      # 判断每个元素是否 > 50
 print(bs1)
 print(s[s > 50])  # 输出满足条件的值
+# II. 是否为null
 bs2 = s.isnull()  # 判断每个元素是否为空值
-print(bs2)
-bs3 = s.notnull()
-print(bs3)
+print(bs2)        # 是 null 的话返回 True
+bs3 = s.notnull() # 判断每个元素是否不是空值
+print(bs3)        # 不是 null 的话返回 True
 print(s[bs3])     # 输出满足条件的值
 
 
@@ -68,30 +65,10 @@ print(s[bs3])     # 输出满足条件的值
 
 # a. 查看数据
 s = pd.Series(np.random.rand(50))
-print(s.head(10)) # 查看头部数据
+print(s.head(3))  # 查看头部数据
 print(s.tail())   # 查看尾部数据, 默认查看5条
 
-# b. 重新索引
-s = pd.Series(np.random.rand(3), index = ['a','b','c'])
-s1 = s.reindex(['c','b','a','d']) # 如果当前索引不存在，则引入缺失值 NaN
-print(s, s1)
-s2 = s.reindex(['c','b','a','d'], fill_value = 0) # fill_value参数：填充缺失值的值
-print(s2)
-
-# c. 对齐
-s1 = pd.Series(np.random.rand(3), index = ['Jack','Marry','Tom'])
-s2 = pd.Series(np.random.rand(3), index = ['Wang','Jack','Marry'])
-print(s1+s2) # series会根据标签自动对齐, 空值和任何值计算结果扔为空值
-
-# d. 删除
-s = pd.Series(np.random.rand(5), index = list('abcde'))
-s1 = s.drop('a')
-s2 = s.drop(['c','e'])
-print(s)
-print(s1)
-print(s2)
-
-# e. 修改
+# b. 修改
 s = pd.Series(np.random.rand(3), index = list('abc'))
 print(s)
 s[2] = 100
@@ -101,7 +78,15 @@ print(s)
 s['a','c'] = 200
 print(s)
 
-# f. 增加
+# c. 删除
+s = pd.Series(np.random.rand(5), index = list('abcde'))
+s1 = s.drop('a')
+s2 = s.drop(['c','e'])
+print(s)
+print(s1)
+print(s2)
+
+# d. 增加数据
 s1 = pd.Series(np.random.rand(3), index = list('abc'))
 print(s1)
 s2 = pd.Series(np.random.rand(2), index = list('de'))
@@ -109,16 +94,29 @@ print(s2)
 s3 = s1.append(s2)
 print(s3)
 
+# e. 连接：concat
+s1 = pd.Series([1,2,3])
+s2 = pd.Series([2,3,4])
+s3 = pd.Series([1,2,3], index = ['a','c','f'])
+s4 = pd.Series([2,3,4], index = ['b','e','d'])
+print(pd.concat([s1,s2]))    # 默认axis=0，行 + 行
+print(pd.concat([s3,s4], axis=1, sort=True))  # axis=1,列+列，成为一个Dataframe
+
+# f. 重新索引
+s = pd.Series(np.random.rand(3), index = ['a','b','c'])
+s1 = s.reindex(['d','c','b','a']) # 如果当前索引不存在，则引入缺失值 NaN
+print(s)
+print(s1)
+s2 = s.reindex(['c','b','a','d'], fill_value = 0) # fill_value参数：填充缺失值的值
+print(s2)
+
 # g. name / rename
 s1 = pd.Series(np.random.randn(5))
-s2 = pd.Series(np.random.randn(5),name = 'test')
+s2 = pd.Series(np.random.randn(5), name = 'test')
 s3 = s2.rename('hehehe')
 print(s1.name, s2.name, s3.name)
 
-# h. 连接：concat
-s1 = pd.Series([1,2,3])
-s2 = pd.Series([2,3,4])
-s3 = pd.Series([1,2,3],index = ['a','c','h'])
-s4 = pd.Series([2,3,4],index = ['b','e','d'])
-print(pd.concat([s1,s2]))    # 默认axis=0，行+行
-print(pd.concat([s3,s4], axis=1, sort=True)) # axis=1,列+列，成为一个Dataframe
+# h. 对齐
+s1 = pd.Series(np.random.rand(3), index = ['Jack','Marry','Tom'])
+s2 = pd.Series(np.random.rand(3), index = ['Wang','Jack','Marry'])
+print(s1+s2) # series会根据标签自动对齐, 空值和任何值计算结果为空值
--- 2. 数据分析与可视化/数据分析/自定义ufunc.py
+++ 2. 数据分析与可视化/数据分析/自定义ufunc.py
@@ -1,13 +1,17 @@
 """ 普通函数转化为 ufunc """
-x = np.arange(0,10000,1)
-# 普通函数
-def square_fn(x, a, b, c):
-    return a * x**2 + b * x + c
+x = np.arange(0,1000000,1)
 
-# 如果要把 x 中的每个函数带入函数，需要循环语句
-%time y = [square_fn(i, 3, 2, 1) for i in x]
+''' 定义函数 '''
+def square_fn(x, a, b):
+    return a * x + b
 
-# 利用 frompyfunc 转化为 ufunc
-np_square_fn = np.frompyfunc(square_fn, 4, 1) # 其中 4 表示函数输入参数为 4 个，1 表示输出参数为 1 个
-%time y = np_square_fn(x, 3, 2, 1)
-# 注: frompyfunc 比循环语句快得多，计算量越大，差距越明显
+''' 如果要把 x 中的每个数带入函数，需用循环语句 '''
+%time print(type([square_fn(i, 2, 1) for i in x]))
+
+''' 
+更好的方法是：利用 frompyfunc 把原始函数转化为 ufunc，
+    好处1：比循环语句快得多，计算量越大，差距越明显；
+    好处2：输出为 ndarray 格式而非 list
+'''
+np_square_fn = np.frompyfunc(square_fn, 3, 1) # 其中 3 表示函数输入参数为 3 个，1 表示输出参数为 1 个
+%time print(type(np_square_fn(x, 2, 1)))
