be0cf1961d4e1b03f77e42bb952e1c17d919e8bf
ZhouBin <zhoubin_chn@163.com>
2019-08-31 09:39:11 +0800

    20190831

---
+++ 4. 机器学习/传统模型/线性模型-分类.py
@@ -0,0 +1,60 @@
+from sklearn import model_selection
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+""" 1. Logistic回归 """
+X, y = load_iris(return_X_y=True)
+
+kfold = model_selection.KFold(n_splits=10, shuffle=True, random_state=2)
+for train,test in kfold.split(X):
+    X_train,X_test = X[train],X[test]
+    y_train,y_test = y[train],y[test]
+    
+    model = LogisticRegression(penalty='l2',  # 指定罚项是 1范数还是 2范数
+                               dual=False,    # 求解对偶形式还是原始形式（当 n_samples > n_features：dual=False）
+                               tol=0.0001,    # 指定收敛与否的阈值
+                               C=1.0,         # 罚项系数的倒数，其值越小，正则化项越大
+                               fit_intercept=True,   # 是否需要计算偏置项
+                               intercept_scaling=1,  
+                               class_weight=None,    # 给出每个分类的权重
+                               random_state=None,    # 设置随机种子
+                               solver='newton-cg',   # 指定优化算法
+                               max_iter=100,         # 指定最大迭代次数
+                               multi_class='multinomial',  # 指定对于多分类问题的策略
+                               verbose=0, 
+                               warm_start=False,     # 是否使用前一次训练结果继续训练
+                               n_jobs=None)   # 指定并行的 CPU 核数
+    
+    model.fit(X_train, y_train)    # 训练模型 
+    print(model.predict(X_test))   # 模型预测
+    print(model.predict_proba(X_test)[0])      # 把 X 预测为各个类型的概率,
+    print(model.predict_log_proba(X_test)[0])  # 把 X 预测为各个类型的概率的对数值
+    print(f'准确率：{model.score(X_test, y_test)}', '\n') # 计算准确率
+
+out = model_selection.cross_val_score(model,X,y,cv=kfold)
+print(f'平均准确率：{out.mean():.4f} ± {out.std():.4f}')
+
+
+""" 2. 线性判别分析 """
+X, y = load_iris(return_X_y=True)
+
+kfold = model_selection.KFold(n_splits=10, shuffle=True, random_state=2)
+for train,test in kfold.split(X):
+    X_train,X_test = X[train],X[test]
+    y_train,y_test = y[train],y[test]
+    
+    model = LinearDiscriminantAnalysis(n_components=None, # 指定降维后的维度，必须小于 n_classes-1
+                                       priors=None,     # 指定每个类别的先验概率
+                                       shrinkage=None,  # 通常在训练样本数少于特征数、solver = lsqr 或 eigen 时使用
+                                       solver='lsqr',   # 指定最优化算法 
+                                       store_covariance=False,  # 计算类别的协方差矩阵
+                                       tol=0.0001) # 用于指定svd算法中收敛的阈值
+    
+    model.fit(X_train, y_train)    # 训练模型 
+    print(model.predict(X_test))   # 模型预测
+    print(model.predict_proba(X_test)[0])      # 把 X 预测为各个类型的概率,
+    print(model.predict_log_proba(X_test)[0])  # 把 X 预测为各个类型的概率的对数值
+    print(f'准确率：{model.score(X_test, y_test)}', '\n') # 计算准确率
+out = model_selection.cross_val_score(model,X,y,cv=kfold)
+print(f'平均准确率：{out.mean():.4f} ± {out.std():.4f}')
