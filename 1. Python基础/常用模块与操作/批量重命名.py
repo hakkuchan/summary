2757e673ac0b2a1782fe10149d0c611dd3a36ded
ZhouBin <zhoubin_chn@163.com>
2019-12-14 13:08:32 +0800

    20191214

--- 1. Python基础/常用模块与操作/批量重命名.py
+++ 1. Python基础/常用模块与操作/批量重命名.py
@@ -1,21 +1,21 @@
 import os
 
 # 设置路径，注意 \\ 和 / 的使用
-path='E:\\Other\\Downloads\\Pic/'      
+path = 'E:\\Other\\Downloads\\Pic/'      
 
 # 提取路径下的所有文件 
-f=os.listdir(path)
+f = os.listdir(path)
 
 # 按照规则重命名
 for num, item in enumerate(f):
-    oldname=path+f[num]
+    oldname = path+f[num]
     if oldname[25:26] == '.':
-        newname= path + '00' + str(oldname[24:25])+'.JPG'  
+        newname = path + '00' + str(oldname[24:25])+'.JPG'  
         os.rename(oldname,newname)
     if oldname[26:27] == '.':
-        newname= path + '0' + str(oldname[24:26])+'.JPG'
+        newname = path + '0' + str(oldname[24:26])+'.JPG'
         os.rename(oldname,newname)
     if oldname[27:28] == '.':
-        newname= path + str(oldname[24:27])+'.JPG'
+        newname = path + str(oldname[24:27])+'.JPG'
         os.rename(oldname,newname)
 print('Complete')
