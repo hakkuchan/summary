00a2b8fee7dc92dfb096afdb97ef9c1f36027564
ZhouBin <zhoubin_chn@163.com>
2019-12-26 18:47:06 +0800

    20191226

---
+++ Python/1. Basics/队列.py
@@ -0,0 +1,47 @@
+""" 1. 概述
+    
+    队列 (queue) 是一个有次序的数据集；
+    
+    新数据项的添加总发生在队列一端；
+    
+    而现存数据项的移除总发生在队列另一端；
+    
+    可以想像成排队，具有先进先出的特性；
+    
+    比如打印任务排队、进程排队等。
+    
+"""
+
+
+""" 2. 基于list实现队列 """
+
+class Queue:
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        ''' 测试是否为空队列 '''
+        return self.items == []
+    
+    def enquene(self, item):
+        ''' 在 queue 首端添加数据 '''
+        self.items.insert(0, item)
+    
+    def dequene(self):
+        ''' 在 queue 尾端移除数据 '''
+        return self.items.pop()
+    
+    def size(self):
+        ''' 返回队列中数据项的个数 '''
+        return len(self.items)
+
+q = Queue()
+print(q.isEmpty())
+q.enquene(4)
+q.enquene('Dog')
+q.enquene(8)
+print(q.size())
+print(q.isEmpty())
+print(q.dequene())
+print(q.dequene())
+print(q.size())
