a54016503f4b4ddcc24e945328e05cf2aef970ac
ZhouBin <zhoubin_chn@163.com>
2019-12-28 09:45:03 +0800

    20191228

--- Python/1. Basics/逻辑值及运算.py
+++ Python/1. Basics/逻辑值及运算.py
@@ -1,54 +1,62 @@
 """ 1. 判断与真值
-        · bool型是int型的子类
-        · bool(0) = False, bool("") = False, bool([]) = False
-        · bool(任何非零数、字符串、非空序列) = True
+    · bool型是int型的子类
+    · bool(0) = False, bool("") = False, bool([]) = False
+    · bool(任何非零数、非空序列) = True
 """
 
-import numpy as np
-a = np.array([1, 2, 3])
-b = np.array([1, 2, 4])
-c = 3
-d = 4
 
 
 """ 2. 逻辑运算 """
+i = 1
+j = 4
+m = [1, 2, 3]
+
 # and 和
-print('and:', (c in a) and (c in b)) # 为了便于可读，用括号括出条件
+print((i in m) and (j in m)) # >>> False
 # or  或
-print('or:', (c in a) or (c in b))
+print((i in m) or (j in m))  # >>> True
 # not 否
-print('not:', c not in b)
+print(i not in m)  # False
+
 
 
 """ 3. 成员运算 """
-# in
-print('in:', d in a)
-# not in
-print('not in:', d not in a)
+i = 1
+m = [1, 2, 3]
 
-# 技巧：用 in 代替 or
+# in
+print(i in m)  # >>> True
+# not in
+print(i not in m)  # >>> False
+
+''' 技巧：用 in 代替 or '''
 x = 1
-if (x == 1) or (x == 2) or (x == 3):
+if (x == 1) or (x == 2) or (x == 3): 
     pass
-# 等价于：  
+# 等价于
 if x in (1, 2, 3):
     pass
 
 
+
 """ 4. 比较运算 """
 # == 等于
-print('==：', a[0] == b[0])
+print([1,2,3] == [1,2,3])  # >>> True
 # != 不等于
-print('!=：', a[0] != b[0])
+print([1,2,3] != [1,2,3]) # >>> False
 # > 大于
-print('>：', a[0] > b[0])
+print(10 > 5)  # >>> True
 # < 小于
-print('<：', a[0] < b[0])
+print(10 < 5)  # >>> False
 # >= 大于等于
-print('>=：', a[0] >= b[0])
+print(6 >= 5)  # >>> True
 # <= 小于等于
-print('>=：', a[0] <= b[0])
+print(5 <= 5)  # >>> True
 
-# 重要：any() 和 all() 比较数组对应元素
-print('any()：', (a == b).any()) # a, b 数组中对应元素只要有相同的就返回 True
-print('all()：', (a == b).all()) # a, b 数组中对应元素必须全部相同才返回 True
+''' 重要：any() 和 all() 比较数组(numpy)对应元素 '''
+import numpy as np
+a = np.array([1, 2, 3])
+b = np.array([1, 2, 4])
+
+print((a == b).any()) # >>> True   对应元素只要有相同的就返回 True
+print((a == b).all()) # >>> Fasle  对应元素必须全部相同才返回 True
