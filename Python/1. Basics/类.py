45b34c1b46df838ccd404c98f991714a343b5e33
ZhouBin <zhoubin_chn@163.com>
2019-12-29 10:22:42 +0800

    20191229

--- Python/1. Basics/类.py
+++ Python/1. Basics/类.py
@@ -4,8 +4,8 @@
       类封装了对象的属性和方法
 
     · 基于类创建对象，也称为实例化，
-      创建对象都自动具备类的通用行为，
-      再根据需要赋予每个对象独特的个性
+      创建的对象都自动具备类的通用行为，
+	  再根据需要赋予每个对象独特的个性
     
     · 定义的类不能直接操作，必须先实例化再操作
 """
@@ -17,8 +17,10 @@
     · 一般形式
     
     class <类名>:  # 类名采用驼峰命名法 
-        # 如需说明文档，编写规则与函数文档相同 
-        def __init__(self, <参数表>):  # 属性函数，实例化时会自动调用该方法，把参数给对象赋值
+        
+		# 如需说明文档，编写规则与函数文档相同 
+        
+		def __init__(self, <参数表>):   # 属性函数，实例化时会自动调用该方法，把参数给对象赋值
             <代码段>        
         def <方法名>(self, <参数表>):   # 方法函数
             <代码段>
@@ -35,12 +37,12 @@ class Car():
    
     # 属性函数
     def __init__(self, maker, model, year): # __init__()初始化函数的作用是初始化属性 maker、model、year
-        self.maker = maker  # self.maker是实参，maker是形参，传递进后面方法函数的参数是self.maker
+        self.maker = maker  # self.maker是实参，maker是形参，传入之后方法函数的参数是self.maker
         self.model = model
         self.year = year
         self.odometer_reading = 0
         
-    # 方法一（描述汽车参数）
+    # 方法一（描述汽车基本参数）
     def describe_car(self):
         print(str(self.year) + ' -- ' + str(self.maker) + ' -- ' + str(self.model))
     
@@ -54,22 +56,21 @@ class Car():
 
         
 
-""" 3. 调用类（创建实例、实例化）
+""" 3. 调用类（也称为 创建实例 或 实例化）
 
     · 一般形式：obj = <类名>(<参数表>)
     
-    · obj.<方法名>() 调用类中的方法
+    · 调用类中的方法：obj.<方法名>() 
 """
     
 # 例：实例化（实例名一般用小写字母）
-car1 = Car('Audi', 'r8', '2018')   # 创建示例 1
-car2 = Car('Benz', 'c300', '2010') # 创建示例 2
-
+car1 = Car('Audi', 'r8', '2018')   # 创建实例 1
 car1.describe_car()  # >>> 2018 -- Audi -- r8
-car2.describe_car()  # >>> 2010 -- Benz -- c300
 
-car1.read_odometer()  # >>> The car has 0 km on it.
-car2.update_odometer(1500)  # 给 self.odometer_reading 赋值 1500
+car2 = Car('Benz', 'c300', '2010') # 创建实例 2
+car2.describe_car()   # >>> 2010 -- Benz -- c300
+car2.read_odometer()  # >>> The car has 0 km on it.
+car2.update_odometer(1500)  # 把 self.odometer_reading 修改为 1500
 car2.read_odometer()  # >>> The car has 15000 km on it.
 
 
@@ -92,6 +93,6 @@ class ElecCar(Car):
         print('Battery remain: '+ str(self.battery) + ' %')
 
 elec_car = ElecCar('Tesla', 'model 3', '2018')
-elec_car.describe_car() 
-elec_car.read_odometer() 
-elec_car.show_battery(50)
+elec_car.describe_car()   # >>> 2018 -- Audi -- model 3
+elec_car.read_odometer()  # >>> The car has 0 km on it.
+elec_car.show_battery(50) # >>> Battery remain: 50%
