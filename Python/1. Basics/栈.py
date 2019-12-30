dedfbfe9c53560c9cf81b3de2bc071596eaea19c
ZhouBin <zhoubin_chn@163.com>
2019-12-30 09:40:28 +0800

    20191229

--- Python/1. Basics/栈.py
+++ Python/1. Basics/栈.py
@@ -37,12 +37,102 @@ class Stack:
         return len(self.items)
 
 s = Stack()
-print(s.isEmpty())
+print(s.isEmpty())  # >>> True
 s.push(5)
 s.push('cat')
 s.push(100)
-print(s.isEmpty())
-print(s.peek())
-print(s.size())
-print(s.pop())
-print(s.pop())
+print(s.isEmpty())  # >>> False
+print(s.peek())     # >>> 100
+print(s.size())     # >>> 3
+print(s.pop())      # >>> 100
+print(s.pop())      # >>> cat
+
+
+
+""" 3. 栈的应用：一般用于倒转次序的操作 """
+
+# 例 1：识别小括号是否左右匹配
+def parchecker(string):
+    s = Stack()
+    balanced = True  # 假定括号匹配成功
+    index = 0
+    while index < len(string) and balanced:
+        symbol = string[index]  # 提取 string 的第 index 个元素
+        if symbol == '(':
+            s.push(symbol)      # 如果是左括号，就将其加入栈
+        else:
+            if s.isEmpty():     # 如果是右括号，且栈空了，说明右括号多了，匹配失败 
+                balanced = False
+            else:               # 如果是右括号，且栈未空，从栈里面移出一个与这个右括号匹配的左括号
+                s.pop()
+        index = index + 1
+    if balanced and s.isEmpty(): # 如果遍历字符串全部元素后，栈空了，匹配成功
+        return True
+    else:                        # 如果遍历字符串全部元素后，栈未空，说明左括号多了
+        return False
+
+print(parchecker('(()(()()))'))  # >>> True
+print(parchecker('(()))'))       # >>> False
+
+
+# 例 2：识别各类括号是否左右匹配
+def parchecker(string):
+    s = Stack()
+    balanced = True  # 假定括号匹配成功
+    index = 0
+    while index < len(string) and balanced:
+        symbol = string[index]  # 提取 string 的第 index 个元素
+        if symbol in '([{':
+            s.push(symbol)      # 如果是左括号，就将其加入栈
+        else:
+            if s.isEmpty():     # 如果是右括号，且栈空了，说明右括号多了，匹配失败 
+                balanced = False
+            else:               # 如果是右括号，且栈未空
+                top = s.pop()   # 从栈顶移出一个元素
+                if not matches(top, symbol):  # 利用matches函数检查括号类型是否匹配
+                    balanced = False
+        index = index + 1
+    if balanced and s.isEmpty(): # 如果遍历字符串全部元素后，栈空了，匹配成功
+        return True
+    else:                        # 如果遍历字符串全部元素后，栈未空，说明左括号多了
+        return False
+def matches(left, right):
+    lefts = '([{'
+    rights = ')]}'
+    return lefts.index(left) == rights.index(right) # 检查左括号和右括号的索引是否一致，一致说明类型匹配
+matches('(',']')
+
+print(parchecker('{[()][]}'))  # >>> True
+print(parchecker('{[(}]}'))    # >>> False
+
+
+# 例 3：十进制转化为二进制
+# 将十进制整数不断除以 2，每次得到的余数就是由低到高的二进制数，输出则是从高到低，所以需要一个栈来反转次序
+def dec_bin(dec_num):
+    remstack = Stack()
+    while dec_num > 0:
+        rem = dec_num % 2       # 取余
+        remstack.push(rem)      # 将余数存入栈
+        dec_num = dec_num // 2  # 整除
+    bin_num = ''
+    while not remstack.isEmpty():
+        bin_num = bin_num + str(remstack.pop())  # 从栈顶依次取出余数，拼接成二进制数
+    return bin_num
+
+print(dec_bin(35)) # >>> 100011
+
+
+# 例 4: 十进制转化为十六位以下的任意进制
+def dec_base(dec_num, base):     # base表示几进制
+    digits = '0123456789ABCDEF'  # 十六进制中，A表示10，B表示11，以此类推，F表示15
+    remstack = Stack()
+    while dec_num > 0:
+        rem = dec_num % base  # 取余
+        remstack.push(rem)    # 将余数存入栈
+        dec_num = dec_num // base  # 整除
+    base_num = ''
+    while not remstack.isEmpty():
+        base_num = base_num + digits[remstack.pop()]  # 从栈顶依次取出余数，拼接成base进制数
+    return base_num
+
+print(dec_base(92, 16)) # >>> 5C
--- Python/1. Basics/队列.py
+++ Python/1. Basics/队列.py
@@ -3,10 +3,11 @@
     · 队列 (queue) 是一个有次序的数据集；
       在队列一端添加数据，在另一端移除数据，
       可以想像成生活中的排队
-	  
-	· 具有先进先出的特性；
+      
+    · 具有先进先出的特性；
       比如打印任务排队、进程排队等
 """
+
 
 
 """ 2. 基于list实现队列 """
@@ -19,11 +20,11 @@ class Queue:
         ''' 测试是否为空队列 '''
         return self.items == []
     
-    def enquene(self, item):
+    def enqueue(self, item):
         ''' 在 queue 首端添加数据 '''
         self.items.insert(0, item)
     
-    def dequene(self):
+    def dequeue(self):
         ''' 在 queue 尾端移除数据 '''
         return self.items.pop()
     
@@ -33,11 +34,33 @@ class Queue:
 
 q = Queue()
 print(q.isEmpty())
-q.enquene(4)
-q.enquene('Dog')
-q.enquene(8)
+q.enqueue(4)
+q.enqueue('Dog')
+q.enqueue(8)
 print(q.size())
 print(q.isEmpty())
-print(q.dequene())
-print(q.dequene())
+print(q.dequeue())
+print(q.dequeue())
 print(q.size())
+
+
+
+""" 3. 应用示例 —— 击鼓传花
+    
+    · 用队列来实现击鼓传花的算法，
+      已知参加游戏的人名列表，每次传花次数固定，传到就出局，
+    
+    · 用算法返回最后剩下的人名
+"""
+
+def deliver(names, num):
+    game_queue = Queue()
+    for name in names:
+        game_queue.enqueue(name)  # 把名字列表中的名字加入队列
+    while game_queue.size() > 1:  # 只要队列中人数超过 1 人，游戏继续
+        for i in range(num):      # 每轮的传递次数为 num 次
+            game_queue.enqueue(game_queue.dequeue())  # 把队尾的人名取出，添加到队首，相当于花传给了队尾的人
+        game_queue.dequeue()      # 每轮结束后，让队尾的人出局
+    return game_queue.dequeue()   # 返回最后剩下的人名
+
+print(deliver(['Amy','Bill','Dave','Eva','Kay','Mike'], 12))
