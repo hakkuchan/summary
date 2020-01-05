3dffbd763653c451cdb270fb3415576e05f87e92
ZhouBin <zhoubin_chn@163.com>
2020-01-05 11:25:42 +0800

    20200105

--- Basics/module_datetime.py
+++ Basics/module_datetime.py
@@ -1,33 +1,53 @@
-""" 日期和时间模块：
-    datetime; dateutil.parser (日期解析方法)
-    主要掌握：datetime.date(), datetime.datetime(), datetime.timedelta()
-"""
-
 import datetime
 from dateutil.parser import parse
 
-''' 1. date '''
-print(datetime.date.today())    # 返回今日日期，输出格式为 date类
-print(datetime.date(1949,10,1)) # (年,月,日) → 直接得到当时日期
+""" 1. datetime.date() : 处理日期 """
+# (1) 返回今日日期（输出格式为date类）
+print(datetime.date.today())    # >>> 2020-01-05
 
-''' 2. datatime '''
-print(datetime.datetime.now())
-print(datetime.datetime(1949,10,1,9,30,0))
-t1, t2 = datetime.datetime(1950,10,1,9,30,0), datetime.datetime(1949,10,1,10,30,0), 
-print(t1-t2)
+# (2) 修改日期格式
+today = datetime.date.today()
+print(today.strftime('%Y.%m.%d'))  # >>> 2020.01.05
 
-''' 3. datetime.timedelta：时间差 '''
+# (3) 返回特定日期
+print(datetime.date(2019,9,1)) # >>> 2019-09-01
+
+
+
+""" 2. datetime.datatime() : 处理日期+时间"""
+# (1) 返回现在时刻
+print(datetime.datetime.now())  # >>> 2020-01-05 10:13:44.223869
+
+# (2) 修改格式
+print(datetime.datetime.now().strftime('%Y.%m.%d %H-%M-%S'))  # >>> 2020.01.05 10-13-44
+
+# (3) 转化为国际标准格式
+now = datetime.datetime.now()
+print(now.isoformat())  # >>> 2020-01-05T10:15:44.271514 (其中的 T 表示 time)
+
+# (4) 返回特定时间
+print(datetime.datetime(2019,9,1,10,28,0))  # >>> 2019-09-01 10:28:00
+
+# (5) 时间运算
+t1 = datetime.datetime(1992,3,13,3,15,0)
+t2 = datetime.datetime(2019,9,1,10,28,0)
+print(abs(t1-t2))  # >>> 10033 days, 7:13:00
+
+
+
+""" 3. datetime.timedelta() : 处理时间间隔 """
 today = datetime.datetime.today()
-yestoday = today - datetime.timedelta(1)
-print(today)
-print(yestoday)
-print(today - datetime.timedelta(7))
+before = today - datetime.timedelta(hours=7)
+yestoday = today - datetime.timedelta(days=1)
+print(today)     # >>> 2020-01-05 10:24:14.269598
+print(before)    # >>> 2020-01-05 03:24:14.269598
+print(yestoday)  # >>> 2020-01-04 10:24:14.269598
 
-'''4. 日期时间解析 '''
-date = '12-21-2017'
-print(parse(date)) # 直接将str转化成datetime.datetime
-print(parse('2000-1-1'))
-print(parse('5/1/2014'))
-print(parse('5/1/2014', dayfirst = True))  # 国际通用格式中，日在月之前，可以通过dayfirst来设置
-print(parse('22/1/2014'))
-print(parse('Jan 31, 1997 10:45 PM'))
+
+
+""" 4. 日期时间解析 : 将str类转化成datetime类 """
+print(parse('2019-9-1'))  # >>> 2019-09-01 00:00:00
+print(parse('1-9-2019 10:28:00'))    # >>> 2019-01-09 10:28:00
+print(parse('1/9/2019', dayfirst = True))   # >>> 2019-09-01 00:00:00
+print(parse('1/9/2019', dayfirst = False))  # >>> 2019-01-09 00:00:00
+print(parse('Sep 1, 2019 10:28 PM'))  # >>> 2019-09-01 22:28:00
