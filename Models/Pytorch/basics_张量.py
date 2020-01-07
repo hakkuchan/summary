98d35c192c2a868c0f2fcfaf3e7f15b6dd5f9111
ZhouBin <zhoubin_chn@163.com>
2020-01-07 09:38:57 +0800

    20200107

--- Models/Pytorch/basics_张量.py
+++ Models/Pytorch/basics_张量.py
@@ -2,22 +2,36 @@ import numpy as np
 import torch 
 import torch.nn as nn
 
-""" numpy 和 tensor 的相互转换 """
-# Creat ndarray data
-data_nd = np.array([[1,2],[3,4],[5,6]]).astype('float')
-# ndarray --> tensor
-data_ts = torch.from_numpy(data_nd) # 无法指定是否可导
-data_ts = torch.tensor(data_nd, requires_grad=False)
-# tensor  --> ndarray
-data_nd = data_ts.numpy()  # 针对 requires_grad=False 的 tensor
-data_nd = data_ts.detach().numpy()  # 针对 requires_grad=True 的 tensor
-# 令 tensor 可导：
+""" 1. 生成 tensor """
+
+# 1.1 list → tensor
 x = torch.tensor([[1.0,2.0],[2.0,3.0]])
-x = x.clone().detach().requires_grad_(True)
+x = x.clone().detach().requires_grad_(True) # requires_grad 用于设定是否可导，True为可导
+
+# 1.2 ndarray → tensor '''
+data_nd = np.array([[1,2],[3,4],[5,6]]).astype('float') # 生成 ndarray 数据
+# 方式 1：
+data_ts = torch.from_numpy(data_nd) # 无法指定是否可导
+# 方式 2：
+data_ts = torch.tensor(data_nd, requires_grad=False) 
 
 
-""" 常用方法 """
-# 行列数
+
+""" 2. tensor → ndarray & scalar """
+nd = np.array([[1, 2]]).astype('float')
+# 方式 1：针对 requires_grad=False 的 tensor
+ts1 = torch.tensor(nd, requires_grad=False) 
+print(ts1.numpy())
+# 方式 2：针对 requires_grad=True 的 tensor
+ts2 = torch.tensor(nd, requires_grad=True)
+print(ts2.detach().numpy())
+# 方式 3：零维 tensor → 标量 (scalar) 
+print(torch.tensor([1.25]).item())
+
+
+
+""" 3. 常用操作 """
+# 显示行列数
 print(data_ts.size())  # size(0): 行数，size(1) 列数
 # 改变行列数
 print(data_ts.view(-1,1))
@@ -29,5 +43,3 @@ print(data_ts[:, 1])
 print(data_ts.type())
 # 求平均值
 print(data_ts.mean())
-# 转化为标量(scalar)，仅对零维矩阵有效
-print(torch.tensor([1.25]).item())
