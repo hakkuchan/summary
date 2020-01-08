9a401ce75aa46e0d2308f8313d4ba5686c342087
ZhouBin <zhoubin_chn@163.com>
2020-01-08 10:21:33 +0800

    20200108

---
+++ Basics/op_进度提示.py
@@ -0,0 +1,19 @@
+""" 1. 基于 progressbar """
+import progressbar
+import time
+
+# 样式 1
+bar = progressbar.ProgressBar()
+for i in bar(range(1000)):
+    time.sleep(0.0001)
+
+# 样式 2  
+bar = progressbar.ProgressBar(widgets=[progressbar.Percentage()])
+for i in bar(range(1000)):
+    time.sleep(0.0001)
+
+
+""" 2. 自制进度提示（推荐） """
+for i in range(1000):
+    time.sleep(0.0001)
+    print(f'进度：{round((i+1)/1000*100, 3)} %', end='\r')
