3a3b37c99db7b0e48ba9ff1dde9dbbe697285aca
ZhouBin <zhoubin_chn@163.com>
2020-01-30 21:26:41 +0800

    20200130

--- Python/DA&Vis/pd_统计.py
+++ Python/DA&Vis/pd_统计.py
@@ -1,7 +1,24 @@
+""" · 目录
+    |
+    |—— 1. 基本统计量
+    |
+    |—— 2. 分组统计
+    |   |
+    |   |—— 2.1 axis方法
+    |   |
+    |   |—— 2.2 按列名分组
+    |   |
+    |   |—— 2.3 group方法
+    |
+    |—— 3. apply(函数)
+    |
+    |—— 4. 透视表
+"""
+
 import numpy as np
 import pandas as pd
 
-''' (1) 内置方法 '''
+""" 1. 基本统计量 """
 df = pd.DataFrame({'x':np.arange(10), 'y':np.arange(91,101), 'z':np.random.random(10)})
 print(df.describe()) # 输出 8 个主要统计量：count, mean, std, min, 25%, 50%, 75%, max 
 print(df.count())    # 统计非 NaN 值的数量
@@ -15,25 +32,27 @@ print(df.skew())     # 偏度（数据的不对称程度）
 print(df.kurt())     # 峰度（正态分布的数据的峰度值为 0；峰度值越显著偏离 0，数据越不服从正态分布。）
 print(df.median())   # 中位数，50%分位数
 print(df.quantile(q=0.75))   # 分位数，参数 q 确定位置
-
 df = pd.DataFrame({'x':[1,2,3,3,4], 'y':[11,11,12,11,13], 'z':[22,12,22,11,22]})
 print(df.mode())    # 众数
-
 df = pd.DataFrame({'x':np.arange(10), 'y':np.arange(91,101), 'z':np.random.random(10)})
 print(df.corr(method='pearson'))  # 求各个 x, y, z 两两之间的 pearson 相关系数
 print(df.corr(method='kendall'))  # 求各个 x, y, z 两两之间的 kendall 相关系数
 print(df.corr(method='pearson'))  # 求各个 x, y, z 两两之间的 spearman 相关系数
 
 
-''' （2）按行列或列名统计（以 mean 为例） '''
+
+""" 2. 分组统计 (统计量以mean为例) """
+
+''' 2.1 axis方法 '''
 df = pd.DataFrame({'x':np.arange(10), 'y':np.arange(91,101), 'z':np.random.random(10)})
 print(df.mean(axis=0))  # axis=0 求每列数据的平均
 print(df.mean(axis=1))  # axis=1 求每行数据的平均
+
+''' 2.2 按列名分组 '''
 print(df['x'].mean())   # 按列名求平均
 print(df[['x','z']].mean())   # 按列名求平均
 
-
-''' (3) 分组统计 '''
+''' 2.3 group方法 '''
 df = pd.DataFrame({'name' : ['A', 'B', 'A', 'B','A', 'A', 'A', 'B'],
                    'var1' : ['1d', '2d', '1d', '3d', '2d', '2d', '1d', '3d'],
                    'var2' : np.random.randn(8),
@@ -44,20 +63,23 @@ print(df.groupby('name').mean())            # 以 name 分组求 mean (基础统
 print(df.groupby(['name','var1']).mean())
 print(df.groupby(['name'])['var3'].mean())  # 以 name 分组，算 var3 的平均值
 
-''' (4) .apply(函数)，对表格中的元素依次进行计算，并输出结果 '''
+
+
+""" 3. apply(函数)：对表格中的元素依次进行计算，并输出结果 """
 df = pd.DataFrame({'x':np.arange(1,3), 'y':np.arange(11,13)})
 print(df.apply(lambda x: x**2))
 print(df['y'].apply(lambda x: x**2))
-
 # 与分组统计结合
-df = pd.DataFrame({'key':['A','B','A','B','A'], 'data1':np.random.rand(5), 'data2':np.random.rand(5)})
+df = pd.DataFrame({'key':['A','B','A','B','A'], 'P1':np.random.rand(5), 'P2':np.random.rand(5)})
 print(df.groupby('key').apply(lambda x: x.describe())) # apply直接运行其中的函数
 # 等价于
 def fn(d):
     return(d.describe())
 print(df.groupby('key').apply(fn))
 
-''' (5) 透视表：pivot_table '''
+
+
+""" 4. 透视表 pivot_table """
 # values：要聚合的列或列的列表
 # index：数据透视表的index，从原数据的列中筛选
 # columns：数据透视表的columns，从原数据的列中筛选
