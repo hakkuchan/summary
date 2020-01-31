279c4cf66e997446832138a18e9528d0e51e93ab
ZhouBin <zhoubin_chn@163.com>
2020-01-31 09:11:59 +0800

    20200131

--- Python/DA&Vis/bokeh_基础.py
+++ Python/DA&Vis/bokeh_基础.py
@@ -1,4 +1,3 @@
-''' bokeh绘图流程 '''
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,36 +6,74 @@ from bokeh.plotting import figure,show # 导入图表绘制、图标展示模块
 from bokeh.io import output_notebook   # 导入notebook绘图模块以确保在 jupyter notebook 中使用
 output_notebook() # notebook绘图命令
 
-# (1) 提取数据
+
+""" 1. 绘图流程：
+
+    · Step 1：准备数据
+    ↓
+    · Step 2：生成画布
+    ↓
+    · Step 3：绘图：图尺寸、设置工具栏、坐标轴名称及范围、图名称
+    ↓
+    · Step 4：显示
+"""
+
+
+''' Step 1： 提取数据 '''
 df = pd.DataFrame(np.random.randn(100,2),columns = ['A','B'])
 
-# (2) 创建绘图空间（画布）
-p = figure(plot_width=600, plot_height=400,         # 图表宽度、高度
+''' Step 2: 生成画布 '''
+p = figure(plot_width=600, plot_height=400,         # 图宽度、高度
            tools = 'pan, wheel_zoom, box_zoom, save, reset,help',  # 设置工具栏，默认全部显示
            toolbar_location='above',                # 工具栏位置："above"，"below"，"left"，"right"
            x_axis_label = 'A', y_axis_label = 'B',  # X,Y轴label
            x_range = [-3,3], y_range = [-3,3],      # X,Y轴范围
-           title="Simple example"                   # 设置图表title
-          )
- 
-# (3) 绘图
+           title="Simple example")                  # 设置图表title
+
+''' Step 3: 绘图（细节设置见第2部分） '''
 p.circle(df['A'], df['B'], size=20, alpha=0.5)
 
-# (4) 显示
+''' Step 4：显示 '''
 show(p)
 
-''' 设置标题：颜色、字体、风格、背景颜色 '''
+
+
+""" 2. 绘图的细节设置
+    |
+    |—— 2.1 标题
+    |
+    |—— 2.2 点线颜色
+    |
+    |—— 2.3 图边框
+    |
+    |—— 2.4 画布背景
+    |
+    |—— 2.5 外边界背景
+    |
+    |—— 2.6 轴线设置
+    |
+    |—— 2.7 轴标签设置
+    |
+    |—— 2.8 背景网格线
+    |
+    |—— 2.9 背景网格线填充
+    |
+    |—— 2.10 图例
+"""
+
+
+''' 2.1 标题 '''
 df = pd.DataFrame(np.random.randn(100,2),columns = ['A','B'])
 p = figure(plot_width=600, plot_height=400, title="Simple example")
 p.circle(df['A'], df['B'], size=20,  alpha=0.5)
-p.title.text_color = "white"
-p.title.text_font = "times"
-p.title.text_font_style = "italic"
-p.title.background_fill_color = "blue"
+p.title.text_color = "white"            # 标题字体颜色
+p.title.text_font = "times"             # 标题字体
+p.title.text_font_style = "italic"      # 标题字体风格
+p.title.background_fill_color = "blue"  # 标题背景颜色
 show(p)
 
 
-''' 颜色设置 ''' 
+''' 2.2 点线颜色 ''' 
 # (1) 147个CSS颜色，参考网址：http://www.colors.commutercreative.com/grid/
 # (2) RGB颜色值，参考网址：https://coolors.co/87f1ff-c0f5fa-bd8b9c-af125a-582b11
 p = figure(plot_width=600, plot_height=400) # 创建绘图空间
@@ -44,7 +81,8 @@ p.circle(df.index, df['A'], color = 'red', size=10,  alpha=0.5)
 p.circle(df.index, df['B'], color = 'blue', size=10,  alpha=0.5)
 show(p)
 
-''' 图表边框线参数设置 ''' 
+
+''' 2.3 图边框 '''
 p = figure(plot_width=600, plot_height=400)
 p.circle(df['A'], df['B'], color = 'green', size=10,  alpha=0.5)
 p.outline_line_width = 10         # 边框线宽
@@ -53,7 +91,8 @@ p.outline_line_color = "navy"     # 边框线颜色
 p.outline_line_dash = [6,4]       # 边框样式
 show(p)
 
-''' 设置绘图空间背景 '''
+
+''' 2.4 画布背景 '''
 p = figure(plot_width=600, plot_height=400)
 p.circle(df.index, df['A'], color = 'green', size=10,  alpha=0.5)
 p.circle(df.index, df['B'], color = '#FF0000', size=10,  alpha=0.5)
@@ -61,7 +100,8 @@ p.background_fill_color = "yellow"    # 绘图空间背景颜色
 p.background_fill_alpha = 0.3        # 绘图空间背景透明度
 show(p)
 
-''' 设置外边界背景 '''
+
+''' 2.5 外边界背景 '''
 p = figure(plot_width=600, plot_height=400)
 p.circle(df.index, df['A'], color = 'green', size=10,  alpha=0.5)
 p.circle(df.index, df['B'], color = '#FF0000', size=10,  alpha=0.5)
@@ -73,40 +113,47 @@ p.min_border_top = 30                 # 外边界背景 - 上宽度
 p.min_border_bottom = 30              # 外边界背景 - 下宽度
 show(p)
 
-''' 轴线设置 '''
-# 轴线标签、轴线线宽、轴线颜色
-# 字体颜色、字体角度
+
+''' 2.6 轴线设置：标签、线宽、颜色、字体颜色、字体角度 '''
 p = figure(plot_width=400, plot_height=400)
 p.circle([1,2,3,4,5], [2,5,8,2,7], size=10)
+
 # 设置x轴线：标签、线宽、轴线颜色
 p.xaxis.axis_label = "Temp"
 p.xaxis.axis_line_width = 3
 p.xaxis.axis_line_color = "red"
+
 # 设置y轴线：标签、字体颜色、字体角度
 p.yaxis.axis_label = "Pressure"
 p.yaxis.major_label_text_color = "orange"
 p.yaxis.major_label_orientation = "vertical"
+
 # 设置刻度
 p.axis.minor_tick_in = 0    # 刻度往绘图区域内延伸长度
 p.axis.minor_tick_out = 5   # 刻度往绘图区域外延伸长度
+
 # 设置轴线范围
 p.xaxis.bounds = (2,4)
 show(p)
 
-''' 轴标签设置 '''
+
+''' 2.7 轴标签设置 '''
 p = figure(plot_width=400, plot_height=400)
 p.circle([1,2,3,4,5], [2,5,8,2,7], size=10)
+
 # 设置标签名称、字体颜色、偏移距离
 p.xaxis.axis_label = "Lot Number"
 p.xaxis.axis_label_text_color = "#aa6666"
 p.xaxis.axis_label_standoff = 30
+
 # 设置标签名称、字体
 p.yaxis.axis_label = "Bin Count"
 p.yaxis.axis_label_text_font_style = "italic"
 p.yaxis.axis_label_text_font_size = "22pt"
 show(p)
 
-''' 背景网格线  '''
+
+''' 2.8 背景网格线  '''
 p = figure(plot_width=600, plot_height=400)
 p.circle(df.index, df['A'], color = '#FF0000', size=10,  alpha=0.5)
 p.xgrid.grid_line_color = None   # 颜色设置，None时则不显示
@@ -116,24 +163,30 @@ p.xgrid.minor_grid_line_color = 'navy'  # minor_line → 设置次轴线
 p.xgrid.minor_grid_line_alpha = 0.1
 show(p)
 
-''' 背景网格线填充 '''
+
+''' 2.9 背景网格线填充 '''
 # 绘制散点图
 p = figure(plot_width=600, plot_height=400)
 p.circle(df.index, df['A'], color = 'green', size=10,  alpha=0.5)
 p.circle(df.index, df['B'], color = '#FF0000', size=10,  alpha=0.5)
+
 # 设置颜色为空
 p.xgrid.grid_line_color = None
+
 # 设置颜色填充，及透明度
 p.ygrid.band_fill_alpha = 0.1
 p.ygrid.band_fill_color = "navy"
+
 # 设置填充边界
 p.grid.bounds = (-1, 1)
 show(p)
 
-''' 图例 '''
+
+''' 2.10 图例 '''
 p = figure(plot_width=600, plot_height=400)
 x = np.linspace(0, 4*np.pi, 100)
 y = np.sin(x)
+
 # 绘制line1，设置图例名称
 p.circle(x, y, legend="sin(x)")
 p.line(x, y, legend="sin(x)")
@@ -146,17 +199,21 @@ p.line(x, 3*y, legend="3*sin(x)", line_color="green")
 # 设置图例位置："top_left"、"top_center"、"top_right" (the default)、"center_right"、"bottom_right"、"bottom_center"
 # "bottom_left"、"center_left"、"center"
 p.legend.location = "bottom_left"
+
 # 设置图例排列方向："vertical" （默认）or "horizontal"
 p.legend.orientation = "vertical"
+
 # 设置图例：字体、风格、颜色、字体大小
 p.legend.label_text_font = "times"
 p.legend.label_text_font_style = "italic"  # 斜体
 p.legend.label_text_color = "navy"
 p.legend.label_text_font_size = '12pt'
+
 # 设置图例外边线：宽度、颜色、透明度
 p.legend.border_line_width = 3
 p.legend.border_line_color = "navy"
 p.legend.border_line_alpha = 0.5
+
 # 设置图例背景：颜色、透明度
 p.legend.background_fill_color = "gray"
 p.legend.background_fill_alpha = 0.2
--- Python/DA&Vis/mpl_基础及点线图.py
+++ Python/DA&Vis/mpl_基础及点线图.py
@@ -1,220 +1,69 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-%matplotlib inline
-from bokeh.plotting import figure,show # 导入图表绘制、图标展示模块
-from bokeh.io import output_notebook   # 导入notebook绘图模块以确保在 jupyter notebook 中使用
-output_notebook() # notebook绘图命令
-
-""" 1. 绘图流程：
-
+""" 绘图流程：
     · Step 1：准备数据
     ↓
     · Step 2：生成画布
     ↓
-    · Step 3：绘图：图尺寸、设置工具栏、坐标轴名称及范围、图名称
+    · Step 3：绘图（以点线图为例）：颜色、点样式、线样式、线宽、图例
     ↓
-    · Step 4：显示
+    · Step 4：修饰：标题、坐标轴、背景网格、标注、图例
 """
 
 
-''' Step 1： 提取数据 '''
-df = pd.DataFrame(np.random.randn(100,2),columns = ['A','B'])
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+
+''' Step 1: 准备数据 '''
+X = np.linspace(-10,10,200)
+Y1 = np.sin(X)
+Y2 = np.cos(X)
+
 
 ''' Step 2: 生成画布 '''
-p = figure(plot_width=600, plot_height=400,         # 图宽度、高度
-           tools = 'pan, wheel_zoom, box_zoom, save, reset,help',  # 设置工具栏，默认全部显示
-           toolbar_location='above',                # 工具栏位置："above"，"below"，"left"，"right"
-           x_axis_label = 'A', y_axis_label = 'B',  # X,Y轴label
-           x_range = [-3,3], y_range = [-3,3],      # X,Y轴范围
-           title="Simple example"                   # 设置图表title
-          )
-
-''' Step 3: 绘图（细节设置见第2部分） '''
-p.circle(df['A'], df['B'], size=20, alpha=0.5)
-
-''' Step 4：显示 '''
-show(p)
+fig = plt.figure(figsize=(8,6))  # figsize 设置画布大小
 
 
-
-""" 2. 绘图的细节设置
-    |
-    |—— 2.1 标题
-    |
-    |—— 2.2 点线颜色
-    |
-    |—— 2.3 图边框
-    |
-    |—— 2.4 画布背景
-    |
-    |—— 2.5 外边界背景
-    |
-    |—— 2.6 轴线设置
-    |
-    |—— 2.7 轴标签设置
-    |
-    |—— 2.8 背景网格线
-    |
-    |—— 2.9 背景网格线填充
-    |
-    |—— 2.10 图例
-"""
+''' Step 3: 画图 (以折线图为例) '''
+ax = fig.add_subplot(1,1,1)
+line1 = ax.plot(X, Y1, 
+                color='blue', # 颜色
+                marker='o',   # 点样式
+                ls = '--',    # 线样式
+                linewidth=1,  # 线宽
+                label='sin')  # 图例
+line2 = ax.scatter(X, Y2, color='red', label='cos', s=1)
+line3 = ax.plot(X, Y1+Y2, color='green', label='sin+cos')
 
 
-''' 2.1 标题 '''
-df = pd.DataFrame(np.random.randn(100,2),columns = ['A','B'])
-p = figure(plot_width=600, plot_height=400, title="Simple example")
-p.circle(df['A'], df['B'], size=20,  alpha=0.5)
-p.title.text_color = "white"            # 标题字体颜色
-p.title.text_font = "times"             # 标题字体
-p.title.text_font_style = "italic"      # 标题字体风格
-p.title.background_fill_color = "blue"  # 标题背景颜色
-show(p)
+''' Step 4: 修饰 '''
 
+# (1) 标题
+ax.set_title('Example', fontsize=16, color='black') 
 
-''' 2.2 点线颜色 ''' 
-# (1) 147个CSS颜色，参考网址：http://www.colors.commutercreative.com/grid/
-# (2) RGB颜色值，参考网址：https://coolors.co/87f1ff-c0f5fa-bd8b9c-af125a-582b11
-p = figure(plot_width=600, plot_height=400) # 创建绘图空间
-p.circle(df.index, df['A'], color = 'red', size=10,  alpha=0.5)
-p.circle(df.index, df['B'], color = 'blue', size=10,  alpha=0.5)
-show(p)
+# (2) 坐标轴
+ax.set_xlabel('X', fontsize=14, color='blue')   # 显示x轴名称
+ax.set_ylabel('Y', fontsize=14, color='blue')   # 显示y轴名称
+ax.set_xlim((-10, 10))  # 设置 x 轴范围
+ax.set_ylim((-2, 2))    # 设置 y 轴范围
+ax.set_xticks(range(-10,10))    # 设置x刻度
+ax.set_yticks([-2,-1,0,1,2])    # 设置y刻度
+ax.tick_params(labelcolor='white', labelsize=14)  # 设置坐标轴样式
+mpl.rcParams['xtick.direction'] = 'in'  # 设置刻度的方向: in,out,inout
+mpl.rcParams['ytick.direction'] = 'out' # 设置刻度的方向，in,out,inout
+# 这里需要导入matploltib
 
+# (3) 背景网格、标注、图例
+ax.grid(True, 
+        linestyle = "--",
+        color = "gray", 
+        linewidth = "0.5",
+        axis = 'both') # x,y,both
+ax.text(-3, 1.5, 'Trigonometrics', fontdict={'size': 14, 'color': 'red'})  
+ax.legend(loc='best', fontsize=14)  # 设置图例
+# 'best'/'upper right'/'upper left'/'lower left'/'lower right'/'right'
+# 'center left'/ 'center right' / 'lower center' / 'upper center' / 'center'
 
-''' 2.3 图边框 '''
-p = figure(plot_width=600, plot_height=400)
-p.circle(df['A'], df['B'], color = 'green', size=10,  alpha=0.5)
-p.outline_line_width = 10         # 边框线宽
-p.outline_line_alpha = 0.5        # 边框线透明度
-p.outline_line_color = "navy"     # 边框线颜色
-p.outline_line_dash = [6,4]       # 边框样式
-show(p)
-
-
-''' 2.4 画布背景 '''
-p = figure(plot_width=600, plot_height=400)
-p.circle(df.index, df['A'], color = 'green', size=10,  alpha=0.5)
-p.circle(df.index, df['B'], color = '#FF0000', size=10,  alpha=0.5)
-p.background_fill_color = "yellow"    # 绘图空间背景颜色
-p.background_fill_alpha = 0.3        # 绘图空间背景透明度
-show(p)
-
-
-''' 2.5 外边界背景 '''
-p = figure(plot_width=600, plot_height=400)
-p.circle(df.index, df['A'], color = 'green', size=10,  alpha=0.5)
-p.circle(df.index, df['B'], color = '#FF0000', size=10,  alpha=0.5)
-p.border_fill_color = "whitesmoke"    # 外边界背景颜色
-p.border_fill_alpha = 0.8             # 外边界透明度
-p.min_border_left = 50                # 外边界背景 - 左边宽度
-p.min_border_right = 50               # 外边界背景 - 右边宽度
-p.min_border_top = 30                 # 外边界背景 - 上宽度
-p.min_border_bottom = 30              # 外边界背景 - 下宽度
-show(p)
-
-
-''' 2.6 轴线设置：标签、线宽、颜色、字体颜色、字体角度 '''
-p = figure(plot_width=400, plot_height=400)
-p.circle([1,2,3,4,5], [2,5,8,2,7], size=10)
-
-# 设置x轴线：标签、线宽、轴线颜色
-p.xaxis.axis_label = "Temp"
-p.xaxis.axis_line_width = 3
-p.xaxis.axis_line_color = "red"
-
-# 设置y轴线：标签、字体颜色、字体角度
-p.yaxis.axis_label = "Pressure"
-p.yaxis.major_label_text_color = "orange"
-p.yaxis.major_label_orientation = "vertical"
-
-# 设置刻度
-p.axis.minor_tick_in = 0    # 刻度往绘图区域内延伸长度
-p.axis.minor_tick_out = 5   # 刻度往绘图区域外延伸长度
-
-# 设置轴线范围
-p.xaxis.bounds = (2,4)
-show(p)
-
-
-''' 2.7 轴标签设置 '''
-p = figure(plot_width=400, plot_height=400)
-p.circle([1,2,3,4,5], [2,5,8,2,7], size=10)
-
-# 设置标签名称、字体颜色、偏移距离
-p.xaxis.axis_label = "Lot Number"
-p.xaxis.axis_label_text_color = "#aa6666"
-p.xaxis.axis_label_standoff = 30
-
-# 设置标签名称、字体
-p.yaxis.axis_label = "Bin Count"
-p.yaxis.axis_label_text_font_style = "italic"
-p.yaxis.axis_label_text_font_size = "22pt"
-show(p)
-
-
-''' 2.8 背景网格线  '''
-p = figure(plot_width=600, plot_height=400)
-p.circle(df.index, df['A'], color = '#FF0000', size=10,  alpha=0.5)
-p.xgrid.grid_line_color = None   # 颜色设置，None时则不显示
-p.ygrid.grid_line_alpha = 0.8    # 设置透明度，虚线设置
-p.ygrid.grid_line_dash = [6, 4]  # dash → 通过设置间隔来做虚线
-p.xgrid.minor_grid_line_color = 'navy'  # minor_line → 设置次轴线
-p.xgrid.minor_grid_line_alpha = 0.1
-show(p)
-
-
-''' 2.9 背景网格线填充 '''
-# 绘制散点图
-p = figure(plot_width=600, plot_height=400)
-p.circle(df.index, df['A'], color = 'green', size=10,  alpha=0.5)
-p.circle(df.index, df['B'], color = '#FF0000', size=10,  alpha=0.5)
-
-# 设置颜色为空
-p.xgrid.grid_line_color = None
-
-# 设置颜色填充，及透明度
-p.ygrid.band_fill_alpha = 0.1
-p.ygrid.band_fill_color = "navy"
-
-# 设置填充边界
-p.grid.bounds = (-1, 1)
-show(p)
-
-
-''' 2.10 图例 '''
-p = figure(plot_width=600, plot_height=400)
-x = np.linspace(0, 4*np.pi, 100)
-y = np.sin(x)
-
-# 绘制line1，设置图例名称
-p.circle(x, y, legend="sin(x)")
-p.line(x, y, legend="sin(x)")
-# 绘制line2，设置图例名称
-p.line(x, 2*y, legend="2*sin(x)",line_dash=[4, 4], line_color="orange", line_width=2)
-# 绘制line3，设置图例名称
-p.square(x, 3*y, legend="3*sin(x)", fill_color=None, line_color="green")
-p.line(x, 3*y, legend="3*sin(x)", line_color="green")
-
-# 设置图例位置："top_left"、"top_center"、"top_right" (the default)、"center_right"、"bottom_right"、"bottom_center"
-# "bottom_left"、"center_left"、"center"
-p.legend.location = "bottom_left"
-
-# 设置图例排列方向："vertical" （默认）or "horizontal"
-p.legend.orientation = "vertical"
-
-# 设置图例：字体、风格、颜色、字体大小
-p.legend.label_text_font = "times"
-p.legend.label_text_font_style = "italic"  # 斜体
-p.legend.label_text_color = "navy"
-p.legend.label_text_font_size = '12pt'
-
-# 设置图例外边线：宽度、颜色、透明度
-p.legend.border_line_width = 3
-p.legend.border_line_color = "navy"
-p.legend.border_line_alpha = 0.5
-
-# 设置图例背景：颜色、透明度
-p.legend.background_fill_color = "gray"
-p.legend.background_fill_alpha = 0.2
-show(p)
+plt.show()
