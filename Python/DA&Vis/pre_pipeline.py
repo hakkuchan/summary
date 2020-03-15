89989cde9d21d9f709b4177469c9eb9425ace167
ZhouBin <zhoubin_chn@163.com>
2020-03-15 18:09:46 +0800

    20200315

--- Python/DA&Vis/pre_pipeline.py
+++ Python/DA&Vis/pre_pipeline.py
@@ -1,30 +1,31 @@
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline,FeatureUnion
-from sklearn.decomposition import PCA
-from sklearn.feature_selection import SelectKBest
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import KFold, cross_val_score
-from numpy import set_printoptions
-set_printoptions(precision=3)
+""" Pipeline
+    
+    · 用于把多个方法组成流水线，比如：数据标准化 → 特征选择 → 预测模型
+    · 除预测模型外，其它方法必须含有transform 或 fit_transform 方法
+"""
 
-name = ['preg','plas','pres','skin','test','mass','pedi','age','class']
-diabetes_table = pd.read_csv('E:\Work\Jupyter\Backup\Data\diabetes\pima-indians-diabetes.csv',names=name)
-diabetes = diabetes_table.values
-X = diabetes[:,0:8]
-Y = diabetes[:,8]
+from sklearn import datasets
+from sklearn import pipeline
+from sklearn import feature_selection
+from sklearn import decomposition
+from sklearn import preprocessing
+from sklearn import svm
+from sklearn import model_selection
 
+# Data：Iris
+X = datasets.load_iris().data
+y = datasets.load_iris().target
+# 特征工程
 features = []
-features.append(('pca',PCA()))
-features.append(('select_best',SelectKBest(k=6)))
-
+features.append(('Select_best', feature_selection.SelectKBest(k=3))) # 选择3个指标
+features.append(('PCA', decomposition.PCA(n_components=2))) # 降维至2个指标
+features.append(('Standardize', preprocessing.StandardScaler())) # 正态分布化
+# 设置流水线步骤
 steps = []
-steps.append(('Standaridize',StandardScaler()))
-steps.append(('feature_union',FeatureUnion(features)))
-steps.append(('LR',LogisticRegression(solver='lbfgs')))
-
-model = Pipeline(steps)
-kfold = KFold(n_splits=10,random_state=1)
-result = cross_val_score(model,X,Y,cv=kfold)
-
-print(result.mean())
+steps.append(('feature_union',pipeline.FeatureUnion(features))) # 特征工程
+steps.append(('Predictive model', svm.SVC(gamma='auto'))) # 预测模型
+# 执行流水线
+pipeline = pipeline.Pipeline(steps) # 此时可把pipeline理解为一个包含流水线功能的大模型
+kfold = model_selection.KFold(n_splits=10,random_state=1)
+result = model_selection.cross_val_score(pipeline,X,y,cv=kfold)
+print('Accuracy:', round(result.mean(),4))
