c95e8fc9ba056a4456c4dfd620636383119d4076
ZhouBin <zhoubin_chn@163.com>
2020-03-19 16:19:04 +0800

    20200319

--- Python/Models/Sklearn/clf_KNN.py
+++ Python/Models/Sklearn/clf_KNN.py
@@ -1,17 +1,17 @@
+""" K Nearest Neighbors Classifier：把未知样本预测为其K近邻中出现的最多的类别 """
+
 import numpy as np
 import pandas as pd
 from sklearn import datasets
 from sklearn import neighbors
 from sklearn import model_selection
 
-''' 准备数据 '''
-db = datasets.load_iris()
-X = db.data
-y = db.target
+# Data：Iris
+X = datasets.load_iris().data
+y = datasets.load_iris().target
 
-''' 建立模型 '''
-model = neighbors.KNeighborsClassifier(
-                                       n_neighbors=5, # 指定k值
+# Modeling
+model = neighbors.KNeighborsClassifier(n_neighbors=5, # 指定 k值
                                        weights='uniform',  # 指定投票的权重类型，uniform指权重相同
                                        algorithm='brute',  # 指点最近邻算法
                                        leaf_size=30,  # 当最近算法为 BallTree 或 KDTree 时，该参数指定叶节点规模
@@ -20,15 +20,6 @@ model = neighbors.KNeighborsClassifier(
                                        n_jobs=-1 # 指定并行数，-1表示派发任务到所有可用CPU核
                                       )
 
-''' 学习及预测 '''
-print('|{:>10s}|{:>10s}|'.format('Batch', 'KNN Acc'))
-kfold = model_selection.KFold(n_splits=10, random_state=1)
-batch = 1
-for train,test in kfold.split(X):
-    X_train, X_test = X[train], X[test]
-    y_train, y_test = y[train], y[test]
-    results = []
-    model.fit(X_train,y_train)
-    results.append(round(model.score(X_test, y_test),4))
-    print('|{:>10.0f}|{:>10.4f}|'.format(batch,results[0]))
-    batch += 1
+# Train & Test
+results = model_selection.cross_val_score(model, X, y, cv=10, scoring='accuracy')
+print(f'Accuracy of KNN : {results.mean()*100:.2f}%')
--- Python/Models/Sklearn/regr_KNN.py
+++ Python/Models/Sklearn/regr_KNN.py
@@ -1,15 +1,17 @@
+""" K Nearest Neighbors Regressor：把未知样本预测为其K近邻y值的均值 """
+
 import numpy as np
 import pandas as pd
 from sklearn import datasets
 from sklearn import neighbors
 from sklearn import model_selection
+from sklearn import metrics
 
-''' 准备数据 '''
-db = datasets.load_diabetes()
-X = db.data
-y = db.target
+# Data：Boston housing price
+X = datasets.load_boston().data
+y = datasets.load_boston().target
 
-''' 建立模型 '''
+# Modeling
 model = neighbors.KNeighborsRegressor(
                                       n_neighbors= 3, # 指定k值
                                       weights='uniform',   # 指定投票的权重类型，uniform指权重相同
@@ -21,15 +23,8 @@ model = neighbors.KNeighborsRegressor(
                                       n_jobs=-1  # 指定并行数，-1表示派发任务到所有可用CPU核
                                      )
 
-''' 学习及预测 '''
-print('|{:>10s}|{:>10s}|'.format('Batch', 'KNN r2'))
-kfold = model_selection.KFold(n_splits=10, random_state=1)
-batch = 1
-for train,test in kfold.split(X):
-    X_train, X_test = X[train], X[test]
-    y_train, y_test = y[train], y[test]
-    results = []
-    model.fit(X_train,y_train)
-    results.append(round(model.score(X_test, y_test),4))
-    print('|{:>10.0f}|{:>10.4f}|'.format(batch,results[0]))
-    batch += 1
+
+# Train & Test
+mse = metrics.make_scorer(metrics.mean_squared_error)
+results = model_selection.cross_val_score(model, X, y, cv=10, scoring=mse)
+print(f'RMSE of KNR : {results.mean()**0.5:.2f}')
