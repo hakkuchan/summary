60ec86a4d6ded7b60befd3e616182dd8041b9b2d
ZhouBin <zhoubin_chn@163.com>
2020-03-22 19:59:11 +0800

    20200322

--- Python/Models/Pytorch/nn_tensorboardX.py
+++ Python/Models/Pytorch/nn_tensorboardX.py
@@ -1,24 +1,44 @@
-""" tensorboard 启动 """
+""" · 目录
+    |
+    |—— 1. 启动
+    |
+    |—— 2. 记录标量
+    |   |
+    |   |—— 2.1 add_scalar()
+    |   |
+    |   |—— 2.2 add_scalars()
+    |
+    |—— 3. 绘制ANN结构 add_graph()
+"""
 
-# 比如日志文件夹的目录是：E:\Work\Jupyter\Data\Log
-# 在上一级目录的地址栏（即 E:\Work\Jupyter\Data）中输入cmd --> 输入命令:tensorboard --logdir=Log
-# 在chrome输入网址： cmd 中给出的网址 或 http://localhost:6006/
 
-
-
-""" tensorboardX 记录标量：add_scalar(), add_scalars() """
-
-""" add_scalar() """
-# 功能：在一个图表中记录一个标量的变化，常用于 Loss 和 Accuracy 曲线的记录。
-# 完整形式：add_scalar(tag, scalar_value, global_step=None, walltime=None)
-# 参数含义：
-# tag(string) - 该图的标签
-# scalar_value(float or string/blobname) - 用于存储的值，曲线图的 y 坐标
-# global_step(int) - 曲线图的 x 坐标
-# walltime(float) - 为 event 文件的文件名设置时间，默认为 time.time()
-
+import numpy as np
 import torch
+import torch.nn as nn
 from tensorboardX import SummaryWriter
+
+
+""" 1. 启动 
+    
+    · 比如日志文件夹的目录是：E:\Work\Jupyter\Data\Log
+    · 在上一级目录的地址栏（即 E:\Work\Jupyter\Data）中输入cmd --> 输入命令:tensorboard --logdir=Log
+    · 在chrome输入网址： cmd 中给出的网址 或 http://localhost:6006/   
+"""
+
+
+""" 2. 记录标量 
+    
+    2.1 add_scalar()
+    
+    · 功能：在一个图表中记录一个标量的变化，常用于 Loss 和 Accuracy 曲线的记录。
+    · 完整形式：add_scalar(tag, scalar_value, global_step=None, walltime=None)
+      参数含义：
+               · tag(string) - 该图的标签
+               · scalar_value(float or string/blobname) - 用于存储的值，曲线图的 y 坐标
+               · global_step(int) - 曲线图的 x 坐标
+               · walltime(float) - 为 event 文件的文件名设置时间，默认为 time.time()
+"""
+
 writer = SummaryWriter('E:\Work\Jupyter\Data\Log') # 创建日志文件夹
 for n_iter in range(100):
     s = torch.rand(1)
@@ -26,32 +46,29 @@ for n_iter in range(100):
 writer.close()
 
 
-""" add_scalars() """
-# 功能：在一个图表中记录多个标量的变化，常用于对比。
-# 完整形式：add_scalars(main_tag,tag_scalar_dict,global_step=None,walltime=None)
-# main_tag(string) - 该图的标签
-# tag.scalar_dict(dict) - dict中，key是变量的tag，value是变量的值
-# global_step(int) - 曲线图的x坐标
-# walltime(float)- 为event文件的文件名设置时间，默认为time.timeO
 
-import numpy as np
-import torch
-from tensorboardX import SummaryWriter
+''' 2.2 add_scalars()
+    
+    · 功能：在一个图表中记录多个标量的变化，常用于对比。
+    · 完整形式：add_scalars(main_tag,tag_scalar_dict,global_step=None,walltime=None)
+      参数含义：
+              · main_tag(string) - 该图的标签
+              · tag.scalar_dict(dict) - dict中，key是变量的tag，value是变量的值
+              · global_step(int) - 曲线图的x坐标
+              · walltime(float)- 为event文件的文件名设置时间，默认为time.timeO
+'''
 writer = SummaryWriter('E:\Work\Jupyter\Data\Log') # 创建日志文件夹
 for n_iter in range(1, 100):
     writer.add_scalars('two trigonometrics', {"xsinx": n_iter * np.sin(n_iter), "xcosx": n_iter * np.cos(n_iter),}, n_iter)
 writer.close()
 
 
-""" tensorboardX 绘制网络结构：add_graph() """
 
-import torch
-import torch.nn as nn
-from tensorboardX import SummaryWriter
-
-class Model(nn.Module):
+""" 3. 绘制ANN结构 add_graph() """
+# 搭建ANN
+class MLP(nn.Module):
     def __init__(self, in_dim, n_hidden, out_dim):
-        super(Model, self).__init__()
+        super(MLP, self).__init__()
         self.layer1 = nn.Sequential(nn.Linear(in_dim, n_hidden),
                                     nn.BatchNorm1d(n_hidden),
                                     nn.ReLU(True))
@@ -60,9 +77,9 @@ class Model(nn.Module):
         out = self.layer1(x)
         out = self.layer2(out)
         return out
-    
+# 绘制ANN结构
 X = torch.rand(100, 10)
-mlp = Model(10, 20, 1)
+model = MLP(10, 20, 1)
 writer = SummaryWriter('E:\Work\Jupyter\Data\Log')  # 创建日志文件夹
-writer.add_graph(mlp, (X,))
+writer.add_graph(model, (X,))
 writer.close()
