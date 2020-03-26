9071dc62426c43a0aab8e3bbe0433bdb63b84707
ZhouBin <zhoubin_chn@163.com>
2020-03-26 15:32:09 +0800

    20200326

--- Python/DA&Vis/stat_group.py
+++ Python/DA&Vis/stat_group.py
@@ -1,24 +1,60 @@
+""" · 目录
+    |
+    |—— 1. axis参数
+    |
+    |—— 2. 按列名
+    |
+    |—— 3. group方法
+    |
+    |—— 4. 透视表 pivot_table()
+"""
+
+
 import numpy as np
 import pandas as pd
 
-""" 2. 分组统计 (统计量以mean为例) """
 
-''' 2.1 axis方法 '''
-df = pd.DataFrame({'x':np.arange(10), 'y':np.arange(91,101), 'z':np.random.random(10)})
+""" 1. axis参数 """
+df = pd.DataFrame({'X':np.arange(0,5), 'Y':np.arange(10,15)})
 print(df.mean(axis=0))  # axis=0 求每列数据的平均
 print(df.mean(axis=1))  # axis=1 求每行数据的平均
 
-''' 2.2 按列名分组 '''
-print(df['x'].mean())   # 按列名求平均
-print(df[['x','z']].mean())   # 按列名求平均
 
-''' 2.3 group方法 '''
+""" 2. 按列名 """
+df = pd.DataFrame({'A':np.arange(0,5), 'B':np.arange(10,15), 'C':np.arange(20,25)})
+print(df['A'].mean()) # 按列名求平均
+print(df[['A','C']].mean()) # 按列名求平均
+
+
+""" 3. group方法 """
 df = pd.DataFrame({'name' : ['A', 'B', 'A', 'B','A', 'A', 'A', 'B'],
                    'var1' : ['1d', '2d', '1d', '3d', '2d', '2d', '1d', '3d'],
                    'var2' : np.random.randn(8),
                    'var3' : np.random.randn(8)})
 print(df)
-print(df.groupby(['name']).get_group('A'))  # .get_group()提取满足条件的组（注意：这种方法不如 df[df['name']=='A'] 方便）
-print(df.groupby('name').mean())            # 以 name 分组求 mean (基础统计里的函数可以直接套用过来)
+# (1) get_group()提取满足条件的组 (注意：这种方法不如 df[df['name']=='A'] 方便)
+print(df.groupby(['name']).get_group('A'))
+# (2) 以 name 分组求 mean
+print(df.groupby('name').mean())          
+# (3) 以 name 分组，输出 var3 的 mean
+print(df.groupby(['name'])['var3'].mean())
+# (4) 先以 name分组，再以 var1分组，求 var2、var3的 mean
 print(df.groupby(['name','var1']).mean())
-print(df.groupby(['name'])['var3'].mean())  # 以 name 分组，算 var3 的平均值
+
+
+""" 4. 透视表 pivot_table() """
+# Toy data
+date = ['2017-5-1','2017-5-2','2017-5-3'] * 3
+date = pd.to_datetime(date)
+df = pd.DataFrame({'date': date, 'name': list('abcabcabc'), 'values': np.random.rand(9)*10})
+print(df)
+
+''' pivot_table()参数用法：
+    · values：被统计的列
+    · index：数据透视表的"行名"，从原数据的列中筛选
+    · columns：数据透视表的"列名"，从原数据的列中筛选
+    · aggfunc：指定函数，支持numpy的ufunc函数
+'''
+
+# 例：统计不同date, name下 values 的 mean
+print(pd.pivot_table(df, values='values', index='date', columns='name', aggfunc=np.mean)) 
--- Python/DA&Vis/stat_index.py
+++ Python/DA&Vis/stat_index.py
@@ -1,22 +1,65 @@
+""" · 目录
+    |
+    |—— 1 ~ 5. 计数、平均值、标准差&方差、最小值&最大值、四分位数
+    |
+    |—— 6 ~ 10. 描述性统计、众数、相关性、偏度、峰度
+"""
+
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame({'x':np.arange(10), 'y':np.arange(91,101), 'z':np.random.random(10)})
-print(df.describe()) # 输出 8 个主要统计量：count, mean, std, min, 25%, 50%, 75%, max 
-print(df.count())    # 统计非 NaN 值的数量
-print(df.min())      # 最小值
-print(df.max())      # 最大值
-print(df.mean())     # 平均值
-print(df.mean(skipna=False)) # skipna参数：是否忽略 NaN，默认True，如 False，有 NaN 的列统计结果仍为NaN
-print(df.std())      # 标准差
-print(df.var())      # 方差
-print(df.skew())     # 偏度（数据的不对称程度）
-print(df.kurt())     # 峰度（正态分布的数据的峰度值为 0；峰度值越显著偏离 0，数据越不服从正态分布。）
-print(df.median())   # 中位数，50%分位数
-print(df.quantile(q=0.75))   # 分位数，参数 q 确定位置
-df = pd.DataFrame({'x':[1,2,3,3,4], 'y':[11,11,12,11,13], 'z':[22,12,22,11,22]})
-print(df.mode())    # 众数
-df = pd.DataFrame({'x':np.arange(10), 'y':np.arange(91,101), 'z':np.random.random(10)})
-print(df.corr(method='pearson'))  # 求各个 x, y, z 两两之间的 pearson 相关系数
-print(df.corr(method='kendall'))  # 求各个 x, y, z 两两之间的 kendall 相关系数
-print(df.corr(method='pearson'))  # 求各个 x, y, z 两两之间的 spearman 相关系数
+# Toy data
+df = pd.DataFrame({'X':(1,2,2,3), 'Y':(5,5,7,8)})
+
+""" 1. 计数(排除NaN值) """
+print(df.count())
+
+
+""" 2. 平均值 """
+print(df.mean())
+print(df.mean(skipna=True)) # 跳过 Null值
+
+
+""" 3. 标准差 & 方差 """
+print(df.std())
+print(df.var())
+
+
+""" 4. 最小值 & 最大值 """
+print(df.min())
+print(df.max())
+
+
+""" 5. 四分位数：把所有数值由小到大排列并分成四等份，处于三个分割点位置的数值
+    
+    · 25%位置上的数值：下四分位数
+      50%位置上的数值：中位数
+      75%位置上的数值：上四分位数
+"""
+print(df.quantile(q=0.25))   # 下分位数，25%分位数
+print(df.median())           # 中位数，50%分位数
+print(df.quantile(q=0.5))    # 中位数，50%分位数
+print(df.quantile(q=0.75))   # 上分位数，75%分位数
+# 注：q也可取其它值
+
+
+""" 6. 描述性统计: count, mean, std, min, 25%(下分位数), 50%(中位数), 75%(上分位数), max """
+print(df.describe())
+
+
+""" 7. 众数 """
+print(df.mode())
+
+
+""" 8. 相关性 """
+print(df.corr(method='pearson'))  # 求各个 X, Y 两两之间的 pearson 相关系数
+print(df.corr(method='kendall'))  # 求各个 X, Y 两两之间的 kendall 相关系数
+print(df.corr(method='pearson'))  # 求各个 X, Y 两两之间的 spearman 相关系数
+
+
+""" 9. 偏度：分布对称的数据偏度为0，左侧拖尾时偏度<0，右侧拖尾时偏度>0 """
+print(df.skew())
+
+
+""" 10. 峰度：正态分布的峰度为0，峰度越偏离0，数据越不服从正态分布 """
+print(df.kurt())
