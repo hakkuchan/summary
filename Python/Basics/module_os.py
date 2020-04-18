8e791af8cb04095f7952d50c9d837ac381e8a5b1
ZhouBin <zhoubin_chn@163.com>
2020-04-18 16:08:23 +0800

    20200418

--- Python/Basics/module_os.py
+++ Python/Basics/module_os.py
@@ -1,40 +1,43 @@
-import os # 常见的文件和目录操作
+""" os模块：主要进行文件的操作 """
 
-# 文件路径格式
-path1 = r'E:\Work\Github\Basics\module_os.py'      # r原始字符串，防止转义
-path2 = 'E:/Work/Github/Basics/module_os.py'       # 单个反斜杠：/
-path3 = 'E:\\Work\\Github\\Basics\\module_os.py'   # 两个斜杠：\\（第一个\是转义符）
+import os
+
+# (1) 文件路径的格式
+path1 = r'E:\Work\Github\Basics\module_os.py'    # r原始字符串，防止转义
+path2 = 'E:/Work/Github/Basics/module_os.py'     # 单个反斜杠：/
+path3 = 'E:\\Work\\Github\\Basics\\module_os.py' # 两个斜杠：\\（第一个\是转义符）
+# 文件路径不能以 \ 结尾，会被理解为转义符
 print(path1)
 print(path2)
 print(path3)
 
-# getcwd() 获取当前工作路径
-now_dir = os.getcwd()
+# (2) getcwd获取当前工作路径
+now_dir = os.getcwd() # cwd: current_work_dir 
 print(now_dir)
 
-# os.listdir() 读取路径下所有文件、文件夹名 '''
-path = r'E:\Work\Github\Basics'
+# (3) listdir读取路径下所有文件和文件夹名 '''
+path = r'E:\Work\Github/'
 f = os.listdir(path)
 for i in f:
     print(i)
-
-# makedirs() 创建路径
-path = r'E:\Work\Jupyter\Data\Test'
+    
+# (4) makedirs 创建路径
+path = r'E:\Work\Jupyter\Data/'
 if not os.path.exists(path):  # 如果目录不存在
     os.makedirs(path)  # 创建目录
     
-# os.path.isdir() 判断某一路径是否是目录 '''
+# (5) os.path.isdir 判断某一路径是否是目录 '''
 print(os.path.isdir(r'E:\Work\Jupyter'))
 print(os.path.isdir(r'E:\Work\Jupyter\trial.ipynb'))
 
-# os.path.isfile() 判断某一路径是否是文件 '''
+# (6) os.path.isfile 判断某一路径是否是文件 '''
 print(os.path.isfile(r'E:\Work\Jupyter'))
 print(os.path.isfile(r'E:\Work\Jupyter\Data\Test\test.txt'))
 
-# os.path.split() 分割目录与文件名
+# (7) os.path.split 分割目录与文件名
 path, filename = os.path.split(r'E:\Work\Jupyter\TEST.ipynb')
 print(path, filename)
 
-# os.path.join() 拼接目录 (不能后接文件)
+# (8) os.path.join 拼接目录 (不能后接文件)
 need_dir = os.path.join('E:','Work','Jupyter')
 print(need_dir)
