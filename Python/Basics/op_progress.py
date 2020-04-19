c4b0e593a74b9c62198aafe0e0794c96b3778a26
ZBTGL <48459915+ZBTGL@users.noreply.github.com>
2020-04-19 09:55:48 +0800

    Update op_progress.py

--- Python/Basics/op_progress.py
+++ Python/Basics/op_progress.py
@@ -1,6 +1,23 @@
-""" 1. 基于 progressbar """
-import progressbar
 import time
+import progressbar
+
+
+""" 1. 自制 (推荐) """
+
+# (1) 只显示进度
+for i in range(1000):
+    time.sleep(0.00001)
+    print(f'进度：{round((i+1)/1000*100, 3)} %', end='\r')
+
+# (2) 进度 + 进度条 + 运行时间
+begin = time.time()
+for i in range(1000):
+    time.sleep(0.00001)
+    run = time.time()
+    print('{:<4} % |{:<30}| {} s'.format(round((i+1)/1000*100,2), int((i+1)/1000*30)*'>', round(run-begin,2)),end='\r')
+
+
+""" 2. progressbar 模块 """
 
 # 样式 1
 bar = progressbar.ProgressBar()
@@ -11,9 +28,3 @@ for i in bar(range(1000)):
 bar = progressbar.ProgressBar(widgets=[progressbar.Percentage()])
 for i in bar(range(1000)):
     time.sleep(0.00001)
-
-
-""" 2. 自制进度提示（推荐） """
-for i in range(1000):
-    time.sleep(0.00001)
-    print(f'进度：{round((i+1)/1000*100, 3)} %', end='\r')
