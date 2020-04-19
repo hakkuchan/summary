0641192e30754256a30b422b448b7220c46bb9bc
ZBTGL <48459915+ZBTGL@users.noreply.github.com>
2020-04-19 09:14:40 +0800

    Update module_itertools.py

--- Python/Basics/module_itertools.py
+++ Python/Basics/module_itertools.py
@@ -1,16 +1,16 @@
 import numpy as np
 import itertools
 
-""" 有序排列 """
+# 有序排列
 for comb in itertools.combinations([1,2,3,4,5], 3):
     print(list(comb))
 print()
 
-""" 全排列 """
+# 全排列
 for comb in itertools.permutations([1,2,3,4,5], 3):
     print(list(comb))
 print()
 
-""" 对若干列表中的元素进行有序排列 """
+# 对若干列表中的元素进行有序排列
 comb = list(itertools.product([1,2,3], [4,5,6], [7,8,9]))
 print(comb)
