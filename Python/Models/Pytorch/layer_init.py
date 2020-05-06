ad5630f9ae28c1df0b2968c921147a5776825f91
ZBTGL <48459915+ZBTGL@users.noreply.github.com>
2020-05-06 15:09:41 +0800

    Update layer_init.py

--- Python/Models/Pytorch/layer_init.py
+++ Python/Models/Pytorch/layer_init.py
@@ -1,9 +1,12 @@
 """ 参数初始化（以内置层 nn.Linear 为例）"""
-# 初始化线性层
+
+# 实例化线性层
 layer = nn.Linear(3,1, bias=True)
+
 # 初始化参数：可选操作，因为调用内置层时会自动初始化
 nn.init.constant_(layer.weight, 1)
 nn.init.constant_(layer.bias, 2)
+
 # 输出参数
 print(layer.state_dict()) # 显示可学习的参数
 print('Weights:', layer.weight.detach())
